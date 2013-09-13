704359095e351e3f00c06a9a1aa76a9a9b271f81
hirakuni45 <hira@rvf-rc45.net>
2013-09-14 00:13:46 +0900

    GUI テスト用アプリケーション本体

---
+++ glfw3_app/gui_test/gui_test.cpp
@@ -0,0 +1,115 @@
+//=====================================================================//
+/*! @file
+	@brief  player クラス
+	@author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include <boost/lexical_cast.hpp>
+#include "gui_test.hpp"
+#include "core/glcore.hpp"
+#include "widgets/widget_utils.hpp"
+#include "widgets/widget_null.hpp"
+#include "widgets/widget_button.hpp"
+#include "widgets/widget_label.hpp"
+#include "widgets/widget_slider.hpp"
+#include "widgets/widget_check.hpp"
+#include "widgets/widget_radio.hpp"
+#include "widgets/widget_list.hpp"
+#include "widgets/widget_image.hpp"
+
+namespace app {
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief  初期化
+	*/
+	//-----------------------------------------------------------------//
+	void gui_test::initialize()
+	{
+		gl::IGLcore* igl = gl::get_glcore();
+
+		using namespace gui;
+		widget_director& wd = director_.at_core().widget_director_;
+
+		if(1) {
+			widget::param wpr(vtx::srect(20, 20, 100, 200), 0);
+			widget_null::param wpr_;
+			widget* root = wd.add_widget<widget_null>(wpr, wpr_);
+			root->set_state(widget::state::POSITION_LOCK);
+
+			widget::param wp(vtx::srect(0, 0, 100, 30), root);
+			widget_radio::param wp_("Enable");
+			wd.add_widget<widget_radio>(wp, wp_);
+			wp.rect_.org.y += 40;
+			wd.add_widget<widget_radio>(wp, wp_);
+			wp.rect_.org.y += 40;
+			wp_.check_ = true;
+			wd.add_widget<widget_radio>(wp, wp_);
+		}
+
+		if(1) {
+			widget::param wp(vtx::srect(30, 300, 150, 40), 0);
+			widget_list::param wp_("List Box");
+			wp_.text_list_.push_back("abc");
+			wp_.text_list_.push_back("1234");
+			wp_.text_list_.push_back("qwert");
+			wd.add_widget<widget_list>(wp, wp_);
+		}
+
+		if(1) {
+			widget::param wp(vtx::srect(200, 20, 500, 500), 0);
+			img::paint pa;
+			pa.set_fore_color(img::rgba8(0, 255, 0));
+			pa.create(vtx::spos(500), true);
+			pa.fill(img::rgba8(255, 100, 100));
+			pa.alpha_blend();
+			pa.fill_circle(vtx::spos(250), 220);
+			vtx::sposs ss;
+			ss.push_back(vtx::spos(10, 10));
+			ss.push_back(vtx::spos(100, 100));
+			ss.push_back(vtx::spos(50, 200));
+			pa.set_fore_color(img::rgba8(240, 40, 50));
+//			pa.fill_polygon(ss);
+			widget_image::param wp_(&pa);
+			wd.add_widget<widget_image>(wp, wp_);
+		}
+
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief  アップデート
+	*/
+	//-----------------------------------------------------------------//
+	void gui_test::update()
+	{
+		gl::IGLcore* igl = gl::get_glcore();
+		const vtx::spos& size = igl->get_size();
+
+		gui::widget_director& wd = director_.at_core().widget_director_;
+		wd.update();
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief  レンダリング
+	*/
+	//-----------------------------------------------------------------//
+	void gui_test::render()
+	{
+		director_.at_core().widget_director_.render();
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief  廃棄
+	*/
+	//-----------------------------------------------------------------//
+	void gui_test::destroy()
+	{
+	}
+
+}
---
+++ glfw3_app/gui_test/main.cpp
@@ -0,0 +1,96 @@
+//=====================================================================//
+/*! @file
+	@brief  player サンプル
+	@author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "main.hpp"
+#include "core/glcore.hpp"
+#include "utils/director.hpp"
+#include "gui_test.hpp"
+#include "gl_fw/glutils.hpp"
+
+#if 0
+void dropfiles(main_core& core)
+{
+	gl::IGLcore* igl = gl::get_glcore();
+
+	int id = igl->get_recv_file_id();
+	if(core.id_ != id) {
+		core.id_ = id;
+		const utils::strings& ss = igl->get_recv_file_path();
+		if(!ss.empty()) {
+		}
+	}
+}
+#endif
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	gl::create_glcore();
+
+	gl::IGLcore* igl = gl::get_glcore();
+
+	// カレントパスを生成
+	std::string tmp;
+	utils::convert_delimiter(argv[0], '\\', '/', tmp);
+	std::string pref;
+	utils::get_file_base(tmp, pref);
+	pref += ".pre";
+	std::string path;
+	utils::get_file_path(tmp, path);
+
+	if(!igl->initialize(path)) {
+		return -1;
+	}
+
+	{
+		utils::director<app::core> director;
+
+		director.at_core().preference_.load(pref);
+
+		if(!igl->setup(vtx::srect(100, 100, 1024, 768), "player", false)) {
+			return -1;
+		}
+		igl->set_limit_size(vtx::spos(1024, 768));
+
+		director.at_core().sound_.initialize(16);
+
+		director.at_core().widget_director_.initialize();
+
+		director.install_scene<app::gui_test>();
+
+		while(!igl->get_exit_signal()) {
+			igl->service();
+
+			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+			gl::glColor(img::rgbaf(1.0f));
+
+//	gl::IGLcore* igl = gl::get_glcore();
+//	const vtx::spos& size = igl->get_size();
+//	core.mobj_bg_.setup_matrix(-size.x / 2, -size.y / 2, size.x / 2, size.y / 2, -1.0f, 1.0f);
+
+			glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+			glEnable(GL_TEXTURE_2D);
+			glEnable(GL_BLEND);
+			glDisable(GL_DEPTH_TEST);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+			director.render();
+
+			igl->flip_frame();
+
+			director.at_core().sound_.service();
+		}
+
+		director.at_core().preference_.save(pref);
+	}
+
+	igl->destroy();
+
+	gl::destroy_glcore();
+
+	return 0;
+}
