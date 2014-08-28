71f5a00061046b54b126f8143b0c6a45f6e79b7e
平松邦仁 <hirakuni45@host59.ad.bexide.jp>
2014-08-28 14:17:34 +0900

    glTexImage2D initail stage

--- glfw3_app/common/gl_fw/glfonts.cpp
+++ glfw3_app/common/gl_fw/glfonts.cpp
@@ -281,11 +281,9 @@ namespace gl {
 
 		int level = 0;
 		if(tmap.lcx == 0 && tmap.lcy == 0) {
-			std::vector<uint8_t> tmp;
-			tmp.resize(texture_page_width * texture_page_height);
-			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 			glTexImage2D(GL_TEXTURE_2D, level,
-				GL_ALPHA, texture_page_width, texture_page_height, 0, GL_ALPHA, GL_UNSIGNED_BYTE, &tmp[0]);
+						 GL_ALPHA, texture_page_width, texture_page_height,
+						 0, GL_ALPHA, GL_UNSIGNED_BYTE, nullptr);
 		}
 
 		{
