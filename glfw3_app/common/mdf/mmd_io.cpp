16767a9b8b29d618f3b78988610dab04fe1ebb74
平松邦仁 <hirakuni45@URANUSMacBook-Pro.local>
2014-10-18 10:53:10 +0900

    add mmd_io class

---
+++ glfw3_app/common/mdf/mmd_io.cpp
@@ -0,0 +1,87 @@
+//=====================================================================//
+/*!	@file
+	@brief	MMD ファイルを扱うクラス
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "mdf/mmd_io.hpp"
+
+namespace mdf {
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief	ロード
+		@param[in]	fio	ファイル入出力クラス
+		@return 成功なら「true」
+	*/
+	//-----------------------------------------------------------------//
+	bool mmd_io::load(utils::file_io& fio)
+	{
+		if(pmd_.load(fio)) {
+			type_ = type::pmd;
+			return true;
+		} else if(pmx_.load(fio)) {
+			type_ = type::pmx;
+			return true;
+		}
+		type_ = type::none;
+		return false;
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief	セーブ
+		@param[in]	fio	ファイル入出力クラス
+		@return 成功なら「true」
+	*/
+	//-----------------------------------------------------------------//
+	bool mmd_io::save(utils::file_io& fio)
+	{
+
+
+		return false;
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief	レンダリング・セットアップ
+	*/
+	//-----------------------------------------------------------------//
+	void mmd_io::render_setup()
+	{
+		if(type_ == type::pmd) pmd_.render_setup();
+		else if(type_ == type::pmx) pmx_.render_setup(); 
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief	レンダリング
+	*/
+	//-----------------------------------------------------------------//
+	void mmd_io::render_surface()
+	{
+		if(type_ == type::pmd) pmd_.render_surface();
+		else if(type_ == type::pmx) pmx_.render_surface(); 
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief	ボーンのフルパスを生成
+		@param[in]	index	ボーンのインデックス
+		@param[out]	path	フル・パスを受け取る参照	
+	*/
+	//-----------------------------------------------------------------//
+	bool mmd_io::create_bone_path(uint32_t index, std::string& path)
+	{
+		if(type_ == type::pmd) pmd_.create_bone_path(index, path);
+		else if(type_ == type::pmx) pmx_.create_bone_path(index, path);
+
+		return false;
+	}
+
+
+}
