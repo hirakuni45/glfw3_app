ff86cb248861d75b24cdbe7c88ad024f2b9fe99e
平松邦仁 <hira@bexide.co.jp>
2015-01-30 19:40:03 +0900

    change path

--- glfw3_app/common/effekseer/gl/Base_Pre.h
+++ glfw3_app/common/effekseer/gl/Base_Pre.h
@@ -6,48 +6,7 @@
 // Include
 //----------------------------------------------------------------------------------
 #include "effekseer/Effekseer.h"
-
-#if defined(__EFFEKSEER_RENDERER_GL_GLEW__)
-
-#if _WIN32
-#include <gl/GL.h>
-#else
-#include <GL/glew.h>
-#endif
-
-#elif defined(__EFFEKSEER_RENDERER_GL_GLEW_S__)
-
-#if _WIN32
-#include <gl/GL.h>
-#else
-#define GLEW_STATIC
-#include <GL/glew.h>
-#endif
-
-
-#elif defined(__EFFEKSEER_RENDERER_GLES2__)
-
-#if defined(__APPLE__)
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
-#else
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-#endif
-
-#else
-
-#if defined(__APPLE__)
-#include <OpenGL/gl3.h>
-#else
-#include <GL/gl.h>
-#endif
-
-#endif
-
-#if _WIN32
-#pragma comment(lib, "gdiplus.lib")
-#endif
+#include "gl_fw/gl_info.hpp"
 
 //----------------------------------------------------------------------------------
 //
