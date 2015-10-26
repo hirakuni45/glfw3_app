dd968d02ef3f0d67381cc7fc1d504758ca9b1e7e
Kunihito Hiramatsu <hira@bexide.co.jp>
2015-10-26 20:22:55 +0900

    Update command

--- glfw3_app/dllcollect/main.cpp
+++ glfw3_app/dllcollect/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <boost/format.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include "utils/sjis_utf16.hpp"
 #include "utils/file_io.hpp"
+
+static const float version_ = 0.01f;
 
 int main(int argc, char* argv[]);
 
@@ -26,6 +29,8 @@ static bool scan_dll_(const std::string& base, const std::string& path, dll_set&
 	cmd += " > " + std::string("dlllist");
 	system(cmd.c_str());
 
+///	std::cout << cmd << std::endl;
+
 	utils::file_io fin;
 	if(!fin.open("dlllist", "rb")) {
 		return false;
@@ -45,16 +50,53 @@ static bool scan_dll_(const std::string& base, const std::string& path, dll_set&
 		}
 	}
 	fin.close();
+
 	return true;
 }
+
+
+static std::string true_path_(const std::string& path)
+{
+	std::string s;
+	utils::code_conv(utils::sjis_to_utf8(path), '\\', '/', s);
+	return s;
+}
+
+
+static void help_(const std::string& cmd)
+{
+	using namespace std;
+
+	auto s = true_path_(cmd);
+
+	cout << "DLL collect copy" << endl;
+	cout << "Copyright (C) 2015, Hiramatsu Kunihito" << endl;
+	cout << "Version " << boost::format("%1.2f") % version_ << endl;
+	cout << "usage:" << endl;
+	string cm;
+	auto n = s.rfind('/');
+	if(n != std::string::npos) { cm = s.substr(n + 1); }
+	else { cm = s; }
+	cout << "    " << utils::get_file_base(cm) << " [options] base-file" << endl;
+//	cout << "    -verbose          verbose" << endl;
+	cout << endl;
+}
+
 
 int main(int argc, char* argv[])
 {
 	utils::init_utf16_to_sjis();
 
-	std::string dll_path("c:/msys64/mingw64/bin");
+	if(argc < 2) {
+		help_(argv[0]);
+		return 0;
+	}
 
-	if(scan_dll_("player.exe", dll_path, dll_set_)) {
+	auto exe = true_path_(argv[1]);
+	std::string dll_path("c:/msys64/mingw64/bin");
+	std::string dir_name("dlls");
+	
+	if(scan_dll_(exe, dll_path, dll_set_)) {
 		size_t n = 0;
 		do {
 			n = dll_set_.size();
@@ -63,9 +105,14 @@ int main(int argc, char* argv[])
 			}
 		} while(n < dll_set_.size()) ;
 
+		system(std::string("rm -rf " + dir_name).c_str());
+		system(std::string("mkdir " + dir_name).c_str());
+
 		for(auto s : dll_set_) {
 			auto path = dll_path + '/' + s;
-			std::cout << " cp " + path + " dlls/." << std::endl;
+			auto copy = "cp " + path + ' ' + dir_name + "/.";
+//			std::cout << copy << std::endl;
+			system(copy.c_str());
 		}
 	}
 }
