be0171c1b61c26d1c6b208640f819ceeee9c0b90
hira <hira@rvf-rc45.net>
2016-12-14 20:14:27 +0900

    update tools

--- glfw3_app/logic_form/logic_form.hpp
+++ glfw3_app/logic_form/logic_form.hpp
@@ -10,8 +10,9 @@
 #include "widgets/widget.hpp"
 #include "widgets/widget_frame.hpp"
 #include "widgets/widget_text.hpp"
-#include "widgets/widget_border.hpp"
+#include "widgets/widget_button.hpp"
 #include "widgets/widget_terminal.hpp"
+#include "widgets/widget_filer.hpp"
 #include "widgets/widget_view.hpp"
 #include "logic.hpp"
 
@@ -22,12 +23,17 @@ namespace app {
 		utils::director<core>&	director_;
 
 		gui::widget_frame*		tools_;
+		gui::widget_button*		load_;
+		gui::widget_button*		save_;
 
 		gui::widget_frame*		project_;
 		gui::widget_view*		view_;
 
 		gui::widget_frame*		terminal_frame_;
 		gui::widget_terminal*	terminal_core_;
+
+		gui::widget_filer*		load_ctx_;
+		gui::widget_filer*		save_ctx_;
 
 		vtx::ipos		view_offset_;
 
@@ -75,7 +81,7 @@ namespace app {
 			for(int i = 0; i < 24; ++i) {
 				rect.org.y = (i + 1) * pin_h_;
 				gui::draw_border(rect);
-				draw_logic_(vtx::irect(pin_n_, i * pin_h_, clip.size.x - pin_n_, logic_lvl_), i);
+				draw_logic_(vtx::irect(pin_n_ + 2, i * pin_h_, clip.size.x - pin_n_, logic_lvl_), i);
 			}
 
 			gui::widget::text_param tp("", img::rgba8(255, 255), img::rgba8(0, 255),
@@ -98,8 +104,10 @@ namespace app {
 		*/
 		//-----------------------------------------------------------------//
 		logic_form(utils::director<core>& d) : director_(d),
-			tools_(nullptr), project_(nullptr), view_(nullptr),
+			tools_(nullptr), load_(nullptr), save_(nullptr),
+			project_(nullptr), view_(nullptr),
 			terminal_frame_(nullptr), terminal_core_(nullptr),
+			load_ctx_(nullptr), save_ctx_(nullptr),
 			view_offset_(0), logic_org_(0)
 		{ }
 
@@ -121,6 +129,7 @@ namespace app {
 		{
 			using namespace gui;
 			widget_director& wd = director_.at().widget_director_;
+			gl::core& core = gl::core::get_instance();
 
 			{	// ツールパレット
 				widget::param wp(vtx::irect(20, 20, 200, 300));
@@ -129,6 +138,17 @@ namespace app {
 				tools_ = wd.add_widget<widget_frame>(wp, wp_);
 				tools_->set_state(gui::widget::state::SIZE_LOCK);
 			}
+			{ // ロード起動ボタン
+				widget::param wp(vtx::irect(10, 20+40*0, 80, 30), tools_);
+				widget_button::param wp_("load");
+				load_ = wd.add_widget<widget_button>(wp, wp_);
+			}
+			{ // セーブ起動ボタン
+				widget::param wp(vtx::irect(10, 20+40*1, 80, 30), tools_);
+				widget_button::param wp_("save");
+				save_ = wd.add_widget<widget_button>(wp, wp_);
+			}
+
 
 			{	// プロジェクト・フレーム
 				widget::param wp(vtx::irect(20, 50, 500, 100));
@@ -137,7 +157,7 @@ namespace app {
 				project_ = wd.add_widget<widget_frame>(wp, wp_);
 			}
 			{   // 描画ビュー
-				widget::param wp(vtx::irect(0, 0, 500, 100), project_);
+				widget::param wp(vtx::irect(0, 50, 500, 100), project_);
 				wp.state_.set(widget::state::CLIP_PARENTS);
 				widget_view::param wp_;
 				wp_.render_func_ = [this](const vtx::irect& clip) {
@@ -147,7 +167,7 @@ namespace app {
 			}
 
 
-			{	// ターミナルのテスト
+			{	// ターミナル
 				{
 					widget::param wp(vtx::irect(20, 100, 100, 200));
 					widget_frame::param wp_;
@@ -159,6 +179,20 @@ namespace app {
 					widget_terminal::param wp_;
 					terminal_core_ = wd.add_widget<widget_terminal>(wp, wp_);
 				}
+			}
+
+			{ // load ファイラー本体
+				widget::param wp(vtx::irect(10, 30, 300, 200));
+				widget_filer::param wp_(core.get_current_path());
+				load_ctx_ = wd.add_widget<widget_filer>(wp, wp_);
+				load_ctx_->enable(false);
+			}
+			{ // save ファイラー本体
+				widget::param wp(vtx::irect(10, 30, 300, 200));
+				widget_filer::param wp_(core.get_current_path());
+				wp_.new_file_ = true;
+				save_ctx_ = wd.add_widget<widget_filer>(wp, wp_);
+				save_ctx_->enable(false);
 			}
 
 			// プリファレンスの取得
@@ -175,11 +209,15 @@ namespace app {
 			if(terminal_frame_ != nullptr) {
 				terminal_frame_->load(pre);
 			}
+			if(load_ctx_ != nullptr) load_ctx_->load(pre);
+			if(save_ctx_ != nullptr) save_ctx_->load(pre);
 
 			// デバッグ
 			logic_.create(2048);
 			logic_.build_clock(0, 1, 3);
 //			logic_.build_clock(0);
+			logic_.build_noise(1);
+			logic_.build_noise(2);
 		}
 
 
@@ -191,6 +229,26 @@ namespace app {
 		void update()
 		{
 			gui::widget_director& wd = director_.at().widget_director_;
+
+			if(load_ != nullptr) {
+				if(load_->get_selected()) {
+					if(load_ctx_) {
+						bool f = load_ctx_->get_state(gui::widget::state::ENABLE);
+						load_ctx_->enable(!f);
+						save_->set_stall(!f);
+					}
+				}
+			}
+
+			if(save_ != nullptr) {
+				if(save_->get_selected()) {
+					if(save_ctx_) {
+						bool f = save_ctx_->get_state(gui::widget::state::ENABLE);
+						save_ctx_->enable(!f);
+						load_->set_stall(!f);
+					}
+				}
+			}
 
 			wd.update();
 		}
@@ -216,6 +274,9 @@ namespace app {
 		void destroy()
 		{
 			sys::preference& pre = director_.at().preference_;
+
+			if(load_ctx_ != nullptr) load_ctx_->save(pre);
+			if(save_ctx_ != nullptr) save_ctx_->save(pre);
 
 			if(terminal_frame_ != nullptr) {
 				terminal_frame_->save(pre);
