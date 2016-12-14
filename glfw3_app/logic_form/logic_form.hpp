eb5db20b26700606ab50781abc2950d942b2ff53
hira <hira@rvf-rc45.net>
2016-12-14 16:45:25 +0900

    update logic project

--- glfw3_app/logic_form/logic_form.hpp
+++ glfw3_app/logic_form/logic_form.hpp
@@ -13,6 +13,7 @@
 #include "widgets/widget_border.hpp"
 #include "widgets/widget_terminal.hpp"
 #include "widgets/widget_view.hpp"
+#include "logic.hpp"
 
 namespace app {
 
@@ -28,37 +29,65 @@ namespace app {
 		gui::widget_frame*		terminal_frame_;
 		gui::widget_terminal*	terminal_core_;
 
+		vtx::ipos		view_offset_;
 
-		static void render_view_(const vtx::irect& clip)
+		logic			logic_;
+
+		static const int pin_n_ = 30;
+		static const int pin_w_ = 30;
+		static const int pin_h_ = 30;
+		static const int logic_ofs_ = 25;
+		static const int logic_lvl_  = 20;
+		static const int logic_step_ = 20;
+
+		void render_logic_(const vtx::irect& rect, int bitpos)
+		{
+			vtx::sposs list;
+//			vtx::ipos p
+			
+			list.emplace_back(rect.org.x,   rect.org.y + logic_ofs_);
+			list.emplace_back(rect.end_x(), rect.org.y + logic_ofs_);
+
+//			bool back = 0;
+//			for(auto v : logic_val_) {
+//				if(v & (1 << bitpos)) {
+//				}
+//			}
+
+			if(list.size() > 2) {
+				gl::glColor(img::rgba8(255, 255));
+				gl::draw_line_strip(list);
+			}
+		}
+
+		// プロジェクトの描画
+		void render_view_(const vtx::irect& clip)
 		{
 			glDisable(GL_TEXTURE_2D);
-			vtx::srect rect(30, 0, 2, clip.size.y);
+			vtx::srect rect(pin_n_, 0, 2, pin_h_ * 24);
 			gui::draw_border(rect);
-
-			gui::widget::text_param tp("A", img::rgba8(255, 255), img::rgba8(0, 255),
-				vtx::placement(vtx::placement::holizontal::LEFT, vtx::placement::vertical::TOP));
 
 			rect.org.x = 0;
 			rect.size.x = clip.size.x;
 			rect.size.y = 2;
 			for(int i = 0; i < 24; ++i) {
-				glDisable(GL_TEXTURE_2D);
-				if(i > 0) {
-					rect.org.y = i * 30;
-					gui::draw_border(rect);
-				}
+				rect.org.y = (i + 1) * pin_h_;
+				gui::draw_border(rect);
+				render_logic_(vtx::irect(pin_n_, i * pin_h_, clip.size.x - pin_n_, pin_h_), i);
 			}
+
+			gui::widget::text_param tp("", img::rgba8(255, 255), img::rgba8(0, 255),
+				vtx::placement(vtx::placement::holizontal::LEFT, vtx::placement::vertical::TOP));
 
 			gl::core& core = gl::core::get_instance();
 			gl::fonts& fonts = core.at_fonts();
 			for(int i = 0; i < 24; ++i) {
-				vtx::irect tr(0, i * 30 + 3, 30, 30);
+				vtx::irect tr(0, i * pin_h_ + 3, pin_n_, pin_h_);
 				tp.set_text(std::to_string(i));
 				gui::draw_text(tp, tr, clip);
 			}
 			fonts.restore_matrix();
 		}
-
 
 	public:
 		//-----------------------------------------------------------------//
@@ -68,7 +97,8 @@ namespace app {
 		//-----------------------------------------------------------------//
 		logic_form(utils::director<core>& d) : director_(d),
 			tools_(nullptr), project_(nullptr), view_(nullptr),
-			terminal_frame_(nullptr), terminal_core_(nullptr)
+			terminal_frame_(nullptr), terminal_core_(nullptr),
+			view_offset_(0)
 		{ }
 
 
@@ -108,7 +138,9 @@ namespace app {
 				widget::param wp(vtx::irect(0, 0, 500, 100), project_);
 				wp.state_.set(widget::state::CLIP_PARENTS);
 				widget_view::param wp_;
-				wp_.render_func_ = render_view_;
+				wp_.render_func_ = [this](const vtx::irect& clip) {
+					render_view_(clip);
+				};
 				view_ = wd.add_widget<widget_view>(wp, wp_);
 			}
 
