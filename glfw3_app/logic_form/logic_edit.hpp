d857bdcb7f2245e86320678b7358b1dfb8211bae
hira <hira@rvf-rc45.net>
2016-12-16 04:53:46 +0900

    update command

--- glfw3_app/logic_form/logic_edit.hpp
+++ glfw3_app/logic_form/logic_edit.hpp
@@ -30,6 +30,19 @@ namespace tools {
 		static const uint32_t ch_limit_ = 24;		///< チャネルの最大値
 		static const uint32_t pos_limit_ = 2046;	///< 波形位置の最大値
 
+		int32_t get_dec_(uint32_t idx, const utils::strings& ss)
+		{
+			int32_t val = -1;
+			if(idx < ss.size() && ss.size() >= (idx + 1)) {
+				val = std::stoi(ss[idx]);
+				if(val >= 0 && val < pos_limit_) {
+				} else {
+					val = -1;
+				}				
+			}
+			return val;
+		}
+
 
 		// カレント・チャネル
 		bool cur_ch_(const utils::strings& ss)
@@ -62,14 +75,8 @@ namespace tools {
 				return true;
 			}
 
-			uint32_t pos = 0;
-			if(ss.size() >= 2) {
-				pos = std::stoi(ss[1]);
-				if(pos < pos_limit_) {
-				} else {
-					return false;
-				}
-			}
+			auto pos = get_dec_(1, ss);
+			if(pos < 0) return false;
 
 			int lvl = 1;
 			if(ss.size() >= 3) {
@@ -90,13 +97,35 @@ namespace tools {
 		bool clock_(const utils::strings& ss)
 		{
 			if(ss[0] == "help") {
-				output("Clock Build: start-position length [low-count=1] [high-count=1]\n");
+				output("Build Clock: start-position length [low-count=1] [high-count=1]\n");
 				return true;
 			}
 
-			if(ss.size() > 1) return false;
+			auto pos = get_dec_(1, ss);
+			if(pos < 0) return false;
 
+			auto len = get_dec_(2, ss);
+			if(len < 0) return false;
 
+			int32_t lc = 1;
+			if(ss.size() >= 4) {
+				lc = std::stoi(ss[3]);
+				if(lc >= 0) {
+				} else {
+					return false;
+				}
+			}
+
+			int32_t hc = 1;
+			if(ss.size() >= 5) {
+				hc = std::stoi(ss[4]);
+				if(hc >= 0) {
+				} else {
+					return false;
+				}
+			}
+
+			logic_.build_clock(ch_, pos, len, lc, hc);
 
 			return true;
 		}
@@ -106,9 +135,26 @@ namespace tools {
 		bool fill_(const utils::strings& ss)
 		{
 			if(ss[0] == "help") {
-				output("Fill logic: start-position length [level=1]\n");
+				output("Fill logic level: start-position length [level=1]\n");
 				return true;
 			}
+
+			auto pos = get_dec_(1, ss);
+			if(pos < 0) return false;
+
+			auto len = get_dec_(2, ss);
+			if(len < 0) return false;
+
+			int32_t lvl = 1;
+			if(ss.size() >= 4) {
+				lvl = std::stoi(ss[3]);
+				if(lvl >= 0 && lvl <= 1) {
+				} else {
+					return false;
+				}
+			}
+
+			logic_.fill(ch_, pos, len, lvl);
 
 			return true;
 		}
@@ -117,18 +163,49 @@ namespace tools {
 		bool flip_(const utils::strings& ss)
 		{
 			if(ss[0] == "help") {
+				output("Flip logic level: start-position length\n");
 				return true;
 			}
 
+			auto pos = get_dec_(1, ss);
+			if(pos < 0) return false;
+
+			auto len = get_dec_(2, ss);
+			if(len < 0) return false;
+
+			logic_.flip(ch_, pos, len);
+
 			return true;
 		}
+
 
 		// コピー
 		bool copy_(const utils::strings& ss)
 		{
 			if(ss[0] == "help") {
+				output("Copy logic level: src-position length dst-position\n");
 				return true;
 			}
+
+			auto src = get_dec_(1, ss);
+			if(src < 0) return false;
+
+			auto len = get_dec_(2, ss);
+			if(len < 0) return false;
+
+			auto dst = get_dec_(3, ss);
+			if(dst < 0) return false;
+
+
+
+			return true;
+		}
+
+
+		// コピー・チャネル
+		bool copy_chanel_(const utils::strings& ss)
+		{
+
 
 			return true;
 		}
@@ -186,6 +263,7 @@ namespace tools {
 					fill_(ss);
 					flip_(ss);
 					copy_(ss);
+					copy_chanel_(ss);
 				} else if(ss.size() == 2) {
 					cmd = ss[1];
 				}
@@ -197,6 +275,7 @@ namespace tools {
 			else if(cmd == "fill") ret = fill_(ss);
 			else if(cmd == "flip") ret = flip_(ss);
 			else if(cmd == "copy") ret = copy_(ss);
+			else if(cmd == "copy_chanel") ret = copy_chanel_(ss);
 
 			if(!ret) {  // error
 				output("Command error: " + line + "\n");
@@ -204,6 +283,7 @@ namespace tools {
 
 			return ret;
 		}
+
 	};
 
 }
