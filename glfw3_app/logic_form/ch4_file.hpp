ac5f718309466c3821da4892f515feacd1ceec24
hira <hira@rvf-rc45.net>
2016-12-20 09:08:30 +0900

    update ch4 format

--- glfw3_app/logic_form/ch4_file.hpp
+++ glfw3_app/logic_form/ch4_file.hpp
@@ -5,7 +5,7 @@
 	@author 平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
-#include "utils/file_io.hpp"
+#include "logic.hpp"
 
 namespace tools {
 
@@ -16,14 +16,42 @@ namespace tools {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	class ch4_file {
 
+		logic&	logic_;
+
 	public:
 		//-------------------------------------------------------------//
 		/*!
 			@brief  コンストラクター
 		*/
 		//-------------------------------------------------------------//
-		ch4_file() { }
+		ch4_file(logic& log) : logic_(log) { }
 
 
+		//-------------------------------------------------------------//
+		/*!
+			@brief  セーブ
+			@param[in]	name	ファイル名
+			@return エラー無ければ「true」
+		*/
+		//-------------------------------------------------------------//
+		bool save(const std::string& name)
+		{
+			if(logic_.size() == 0) return false;
+
+			utils::file_io fio;
+			if(!fio.open(name, "wb")) {
+				return false;
+			}
+
+			for(uint32_t i = 0; i < logic_.size(); ++i) {
+				auto bits = logic_.get(i);
+				auto s = (boost::format("%06X") % bits).str();
+				fio.put("0x" + s + "\n");
+			}
+
+			fio.close();
+
+			return true;
+		}
 	};
 }
