72acfdd8a4513136ea84705221e15f4833b8fe75
hira <hira@rvf-rc45.net>
2017-02-17 04:40:21 +0900

    new file

---
+++ glfw3_app/common/utils/format.hpp
@@ -0,0 +1,602 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  format クラス @n
+			・安全性を考慮した、printf 表示に準じたクラス
+			・二進表記として、「%b」をサポート
+			・固定小数点表示「%N.M:Ly」形式をサポート @n
+			※ N：全桁数、M：小数部桁数、L：小数部のビット数 @n
+			※ N には、小数点、符号が含まれる @n
+			Ex: %1.2:8y ---> 256 で 1.00、128 で 0.50、384 で 1.50 と @n
+			と表示される。
+			Copyright 2013,2016 Kunihito Hiramatsu
+    @author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include <type_traits>
+#include <unistd.h>
+
+/* 
+  e, E
+     double 引き数を丸めて [-]d.ddde±dd の形に変換する。 小数点の前には一桁の数字があり、
+     小数点以下の桁数は精度で指定された桁数 になる。精度は指定されなかった場合 6 とみなされる。
+     精度が 0 の場合には、 小数点以下は表示されない。E 変換では、指数を表現するときに 
+     (e で はなく) E が使われる。指数部分は少なくとも 2桁表示される。つまり、 指数の値が 0
+     の場合には、00 と表示される。 
+
+  f, F
+    double 引き数を丸めて [-]ddd.ddd の形の10進表現に変換する。 小数点の後の桁数は、精度で
+    指定された値となる。 精度が指定されていない場合には 6 として扱われる。 精度として明示的
+    に 0 が指定されたときには、小数点以下は表示されない。 小数点を表示する際には、小数点の
+    前に少なくとも一桁は数字が表示される。
+
+    (SUSv2 では、F は規定されておらず、無限や NaN に関する文字列表現を 行ってもよいことになっている。
+     C99 標準では、f 変換では、無限は "[-]inf" か "[-]infinity" と表示し、 NaN は文字列の先頭
+    に `nan をつけて表示するように規定されている。 F 変換の場合は
+    "[-]INF", "[-]INFINITY", "NAN*" と表示される。) 
+
+  g, G
+    double 引き数を f か e (G 変換の場合は F か E) の形式に変換する。 精度は表示する桁数を
+    指定する。 精度が指定されない場合は、6桁とみなされる。 精度が 0 の場合は、1桁とみなされる。
+    変換される値の指数が、 -4 より小さいか、精度以上の場合に、 e 形式が使用される。
+    変換された結果の小数部分の末尾の 0 は削除される。小数点が表示されるのは、 小数点以下に
+    数字が少なくとも一つある場合にだけである。 
+*/
+
+namespace utils {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  標準出力ファンクタ
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class def_chaout {
+	public:
+		void operator() (char ch) {
+			char tmp = ch;
+			write(1, &tmp, 1);
+		}
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  簡易 format クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class chaout>
+	class basic_format {
+	public:
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  エラー種別
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class error : uint8_t {
+			none,		///< エラー無し
+			null,		///< 無効なポインター
+			unknown,	///< 不明な「型」
+			different,	///< 異なる「型」
+		};
+
+	private:
+		chaout	chaout_;
+
+		enum class mode : uint8_t {
+			CHA,		///< 文字
+			STR,		///< 文字列
+			BINARY,		///< ２進
+			OCTAL,		///< ８進
+			DECIMAL,	///< １０進
+			U_DECIMAL,	///< １０進（符号無し）
+			HEX_CAPS,	///< １６進（大文字）
+			HEX,		///< １６進（小文字）
+			FIXED_REAL,	///< 固定小数点
+			REAL,		///< 浮動小数点
+			EXPONENT_CAPS,	///< 浮動小数点 exp 形式(E)
+			EXPONENT,	///< 浮動小数点 exp 形式(e)
+			REAL_AUTO,	///< 浮動小数点自動
+			NONE		///< 不明
+		};
+
+		error		error_;
+
+		const char*	form_;
+
+		char		buff_[34];
+
+		uint8_t		num_;
+		uint8_t		point_;
+		uint8_t		bitlen_;
+		mode		mode_;
+		bool		zerosupp_;
+		bool		sign_;
+
+		void str_(const char* str) {
+			char ch;
+			while((ch = *str++) != 0) chaout_(ch);
+		}
+
+		void reset_() {
+			num_ = 0;
+			point_ = 0;
+			bitlen_ = 0;
+			mode_ = mode::NONE;
+			zerosupp_ = false;
+			sign_ = false;
+		}
+
+		void next_() {
+			enum class apmd : uint8_t {
+				none,
+				num,    // 数字
+				point,	// 小数点
+				bitlen,	// 固定小数点、ビット長さ
+			};
+
+			if(form_ == nullptr) {
+				error_ = error::null;
+				return;
+			}
+			char ch;
+			apmd md = apmd::none;
+			while((ch = *form_++) != 0) {
+				if(md != apmd::none) {
+					if(ch == '+') {
+						sign_ = true;
+					} else if(ch >= '0' && ch <= '9') {
+						ch -= '0';
+						if(md == apmd::num) {
+							if(num_ == 0 && ch == 0) {
+								zerosupp_ = true;
+							}
+							num_ *= 10;
+							num_ += static_cast<uint8_t>(ch);
+						} else if(md == apmd::point) {
+							point_ *= 10;
+							point_ += static_cast<uint8_t>(ch);
+						} else if(md == apmd::bitlen) {
+							bitlen_ *= 10;
+							bitlen_ += static_cast<uint8_t>(ch);
+						}
+					} else if(ch == '.') {
+						md = apmd::point;
+					} else if(ch == ':') {
+						md = apmd::bitlen;
+					} else if(ch == 's') {
+						mode_ = mode::STR;
+						return;
+					} else if(ch == 'c') {
+						mode_ = mode::CHA;
+						return;
+					} else if(ch == 'b') {
+						mode_ = mode::BINARY;
+						return;
+					} else if(ch == 'o') {
+						mode_ = mode::OCTAL;
+						return;
+					} else if(ch == 'd') {
+						mode_ = mode::DECIMAL;
+						return;
+					} else if(ch == 'u') {
+						mode_ = mode::U_DECIMAL;
+						return;
+					} else if(ch == 'x') {
+						mode_ = mode::HEX;
+						return;
+					} else if(ch == 'X') {
+						mode_ = mode::HEX_CAPS;
+						return;
+					} else if(ch == 'y') {
+						mode_ = mode::FIXED_REAL;
+						return;
+					} else if(ch == 'f' || ch == 'F') {
+						mode_ = mode::REAL;
+						return;
+					} else if(ch == 'e') {
+						mode_ = mode::EXPONENT;
+						return;
+					} else if(ch == 'E') {
+						mode_ = mode::EXPONENT_CAPS;
+						return;
+					} else if(ch == 'g' || ch == 'G') {
+						mode_ = mode::REAL_AUTO;
+						return;
+					} else if(ch == '%') {
+						chaout_(ch);
+						md = apmd::none;
+					} else if(ch == '-') {  // 無視する
+
+					} else {
+						error_ = error::unknown;
+						return;
+					}
+				} else if(ch == '%') {
+					md = apmd::num;
+				} else {
+					chaout_(ch);
+				}
+			}
+		}
+
+
+		void out_str_(const char* str, char sign, uint8_t n) {
+			if(zerosupp_) {
+				if(sign != 0) { chaout_(sign); }
+			}
+			if(n && n < num_) {
+				uint8_t spc = num_ - n;
+				while(spc) {
+					--spc;
+					if(zerosupp_) chaout_('0');
+					else chaout_(' ');
+				}
+			}
+			if(!zerosupp_) {
+				if(sign != 0) { chaout_(sign); }
+			}
+			str_(str);
+		}
+
+
+		void out_bin_(int32_t v) {
+			char* p = &buff_[sizeof(buff_) - 1];
+			*p = 0;
+			uint8_t n = 0;
+			do {
+				--p;
+				*p = (v & 1) + '0';
+				v >>= 1;
+				++n;
+			} while(v != 0) ;
+			out_str_(p, '+', n);
+		}
+
+
+		void out_oct_(int32_t v) {
+			char* p = &buff_[sizeof(buff_) - 1];
+			*p = 0;
+			uint8_t n = 0;
+			do {
+				--p;
+				*p = (v & 3) + '0';
+				v >>= 3;
+				++n;
+			} while(v != 0) ;
+			out_str_(p, '+', n);
+		}
+
+
+		void out_udec_(uint32_t v, char sign) {
+			char* p = &buff_[sizeof(buff_) - 1];
+			*p = 0;
+			uint8_t n = 0;
+			do {
+				--p;
+				*p = (v % 10) + '0';
+				v /= 10;
+				++n;
+			} while(v != 0) ;
+			out_str_(p, sign, n);
+		}
+
+
+		void out_dec_(int32_t v) {
+			char sign = 0;
+			if(v < 0) { v = -v; sign = '-'; }
+			else if(sign_) { sign = '+'; }
+			out_udec_(v, sign);
+		}
+
+
+		void out_hex_(uint32_t v, char top) {
+			char* p = &buff_[sizeof(buff_) - 1];
+			*p = 0;
+			uint8_t n = 0;
+			do {
+				--p;
+				char ch = v & 15;
+				if(ch >= 10) ch += top - 10;
+				else ch += '0';
+				*p = ch;
+				v >>= 4;
+				++n;
+			} while(v != 0) ;
+			out_str_(p, 0, n);
+		}
+
+
+		void decimal_(int32_t val, bool sign) {
+			switch(mode_) {
+			case mode::BINARY:
+				out_bin_(val);
+				break;
+			case mode::OCTAL:
+				out_oct_(val);
+				break;
+			case mode::DECIMAL:
+				out_dec_(val);
+				break;
+			case mode::U_DECIMAL:
+				out_udec_(val, sign_ ? '+' : 0);
+				break;
+			case mode::HEX:
+				out_hex_(static_cast<uint32_t>(val), 'a');
+				break;
+			case mode::HEX_CAPS:
+				out_hex_(static_cast<uint32_t>(val), 'A');
+				break;
+			case mode::FIXED_REAL:
+				if(num_ == 0) num_ = 6;
+				if(val < 0) {
+					sign = true;
+					val = -val;
+				}
+				out_fixed_point_<uint64_t>(val, bitlen_, sign);
+				break;
+			default:
+				error_ = error::different;
+				break;
+			}
+		}
+
+
+		uint64_t make_mask_(uint8_t num) {
+			uint64_t m = 0;
+			while(num > 0) {
+				m += m;
+				++m;
+				--num;
+			}
+			return m;
+		}
+
+		template <typename VAL>
+		void out_fixed_point_(VAL v, uint8_t fixpoi, bool sign)
+		{
+// std::cout << "Shift: " << static_cast<int>(fixpoi) << std::endl;
+			// 四捨五入処理用 0.5
+			VAL m = 0;
+			if(fixpoi < (sizeof(VAL) * 8 - 4)) {
+				m = static_cast<VAL>(5) << fixpoi;
+				uint8_t n = point_ + 1;
+				while(n > 0) {
+					m /= 10;
+					--n;
+				}
+			}
+			char sch = 0;
+			if(sign) sch = '-';
+			else if(sign_) sch = '+';
+			v += m;
+			if(num_ >= point_) num_ -= point_;
+			if(num_ > 0 && sch != 0) --num_;
+			if(num_ > 0 && point_ != 0) {
+				--num_;
+			}
+			if(fixpoi < (sizeof(VAL) * 8 - 4)) {
+				out_udec_(v >> fixpoi, sch);
+			} else {
+				out_udec_(0, sch);
+			}
+
+			if(point_ == 0) return;
+			chaout_('.');
+
+			uint8_t l = 0;
+			if(fixpoi < (sizeof(VAL) * 8 - 4)) {
+				VAL dec = v & make_mask_(fixpoi);
+				while(dec > 0) {
+					dec *= 10;
+					VAL n = dec >> fixpoi;
+					chaout_(n + '0');
+					dec -= n << fixpoi;
+					++l;
+					if(l >= point_) break;
+				}
+			}
+			while(l < point_) {
+				chaout_('0');
+				++l;
+			}
+		}
+
+
+		void out_real_(float v, char e) {
+			void* p = &v;
+			uint32_t fpv = *(uint32_t*)p;
+			bool sign = fpv >> 31;
+			int16_t exp = (fpv >> 23) & 0xff;
+			if(exp == 0xff) {
+				if(sign) chaout_('-');
+				str_("inf");
+				return;
+			}
+
+			exp -= 127;	// bias (-127)
+			int32_t val = fpv & 0x7fffff;	// 23 bits
+			int16_t shift = 23;
+			if(val == 0 && exp == -127) ; // [0.0]
+			else {
+				val |= 0x800000; // add offset 1.0
+			}
+			shift -= exp;
+
+			// 64 ビットに拡張
+			uint64_t v64 = static_cast<uint64_t>(val);
+			if(shift < 28) {
+				shift += 32;
+				v64 <<= 32;
+			}
+
+			// エキスポーネント表記の場合
+			int8_t dexp = 0;
+			if(e != 0) {
+				if(v64 > (static_cast<uint64_t>(2) << shift)) {  // 2.0 以上の場合
+					while(v64 > (static_cast<uint64_t>(2) << shift)) {
+						v64 /= 10;
+						++dexp;
+					}
+				} else if(v64 < (static_cast<uint64_t>(1) << shift)) {  // 1.0 以下
+					while(v64 < (static_cast<uint64_t>(1) << shift)) {
+						v64 *= 10;
+						--dexp;
+					}
+				}
+			}
+
+			out_fixed_point_<uint64_t>(v64, shift, sign);
+
+			if(e) {
+				chaout_(e);
+				zerosupp_ = true;
+				sign_ = true;
+				num_ = 3;
+				out_dec_(dexp);
+			}
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+			@param[in]	form	フォーマット式
+		*/
+		//-----------------------------------------------------------------//
+		basic_format(const char* form) noexcept : error_(error::none), form_(form), num_(0), point_(0),
+			bitlen_(0),
+			mode_(mode::NONE), zerosupp_(false), sign_(false) {
+			next_();
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  エラー種別を返す
+			@return エラー
+		*/
+		//-----------------------------------------------------------------//
+		error get_error() const noexcept { return error_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  変換ステータスを返す
+			@return 変換が全て正常なら「true」
+		*/
+		//-----------------------------------------------------------------//
+		bool status() const noexcept { return error_ == error::none; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  オペレーター「%」(const char*)
+			@param[in]	val	値
+			@return	自分の参照
+		*/
+		//-----------------------------------------------------------------//
+		basic_format& operator % (const char* val) noexcept
+		{
+			if(error_ != error::none) {
+				return *this;
+			}
+
+			if(mode_ == mode::STR) {
+				zerosupp_ = false;
+				uint8_t n = 0;
+				const char* p = val;
+				while((*p++) != 0) { ++n; }
+				out_str_(val, 0, n);
+			} else {
+				error_ = error::different;
+			}
+
+			reset_();
+			next_();
+			return *this;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  オペレーター「%」(const char*)
+			@param[in]	val	値
+			@return	自分の参照
+		*/
+		//-----------------------------------------------------------------//
+		basic_format& operator % (char* val) noexcept
+		{
+			if(error_ != error::none) {
+				return *this;
+			}
+
+			if(mode_ == mode::STR) {
+				zerosupp_ = false;
+				uint8_t n = 0;
+				const char* p = val;
+				while((*p++) != 0) { ++n; }
+				out_str_(val, 0, n);
+			} else {
+				error_ = error::different;
+			}
+
+			reset_();
+			next_();
+			return *this;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  オペレーター「%」
+			@param[in]	val	値
+			@return	自分の参照
+		*/
+		//-----------------------------------------------------------------//
+		template <typename T>
+		basic_format& operator % (T val) noexcept
+		{
+			if(error_ != error::none) {
+				return *this;
+			}
+
+			if(std::is_integral<T>::value) {
+				if(mode_ == mode::CHA && sizeof(T) == 1) {
+					chaout_(val);
+				} else {
+					decimal_(static_cast<int32_t>(val), std::is_signed<T>::value);
+				}
+			} else if(std::is_floating_point<T>::value) {
+				if(num_ == 0 && !zerosupp_ && point_ == 0) {
+					num_ = 6;
+					point_ = 6;
+				}
+				switch(mode_) {
+				case mode::REAL:
+					out_real_(val, 0);
+					break;
+				case mode::EXPONENT_CAPS:
+					out_real_(val, 'E');
+					break;
+				case mode::EXPONENT:
+					out_real_(val, 'e');
+					break;
+				case mode::REAL_AUTO:
+					out_real_(val, 0);
+					break;
+				default:
+					error_ = error::different;
+					break;
+				}
+			} else {
+				error_ = error::unknown;
+			}
+
+			reset_();
+			next_();
+			return *this;
+		}
+	};
+
+	typedef basic_format<def_chaout> format;
+}
---
+++ glfw3_app/common/utils/input.hpp
@@ -0,0 +1,402 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  input クラス @n
+			数値、文字列などの入力クラス @n
+			%b ---> ２進の数値 @n
+			%o ---> ８進の数値 @n
+			%d ---> １０進の数値 @n
+			%x ---> １６進の数値 @n
+			%f ---> 浮動小数点数（float、double） @n
+			%c ---> １文字のキャラクター @n
+			%% ---> '%' のキャラクター
+			Copyright 2017 Kunihito Hiramatsu
+    @author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include <type_traits>
+#include <unistd.h>
+
+namespace utils {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  標準入力ファンクタ
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class def_chainp {
+		const char* str_;
+		char		last_;
+		bool		unget_;
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+			@param[in]	str		入力文字列（省力された場合「sci_getch」を使う）
+		*/
+		//-----------------------------------------------------------------//
+		def_chainp(const char* str = nullptr) : str_(str), last_(0), unget_(false) { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  １文字戻す
+		*/
+		//-----------------------------------------------------------------//
+		void unget() {
+			unget_ = true;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ファンクタ（文字取得）
+			@return 文字
+		*/
+		//-----------------------------------------------------------------//
+		char operator() () {
+			if(unget_) {
+				unget_ = false;
+			} else {
+				if(str_ == nullptr) {
+					char ch;
+					if(read(0, &ch, 1) == 1) {
+						if(ch == '\n') ch = 0;
+						last_ = ch;
+					} else {
+						last_ = 0;
+					}
+				} else {
+					last_ = *str_;
+					if(last_ != 0) { ++str_; }
+				}
+			}
+			return last_;
+		}
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  汎用入力クラス
+		@param[in]	INP	文字入力クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class INP>
+	class basic_input {
+	public:
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  エラー種別
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class error : uint8_t {
+			none,			///< エラー無し
+			cha_sets,		///< 文字セットの不一致
+			partition,		///< 仕切りキャラクターの不一致
+			input_type,		///< 無効な入力タイプ
+			not_integer,	///< 整数の不一致
+			not_float,		///< 浮動小数点の不一致
+			terminate,		///< 終端文字の不一致
+		};
+
+	private:
+		const char*	form_;
+
+		INP			inp_;
+
+		enum class mode : uint8_t {
+			NONE,
+			BIN,
+			OCT,
+			DEC,
+			HEX,
+			REAL,
+			CHA,
+		};
+		mode	mode_;
+		error	error_;
+		int		num_;
+
+		uint32_t bin_() {
+			uint32_t a = 0;
+			char ch;
+			while((ch = inp_()) != 0 && ch >= '0' && ch <= '1') {
+				a <<= 1;
+				a += ch - '0';
+			}
+			return a;
+		}
+
+		uint32_t oct_() {
+			uint32_t a = 0;
+			char ch;
+			while((ch = inp_()) != 0 && ch >= '0' && ch <= '7') {
+				a <<= 3;
+				a += ch - '0';
+			}
+			return a;
+		}
+
+		uint32_t dec_() {
+			uint32_t a = 0;
+			char ch;
+			while((ch = inp_()) != 0 && ch >= '0' && ch <= '9') {
+				a *= 10;
+				a += ch - '0';
+			}
+			return a;
+		}
+
+		uint32_t hex_() {
+			uint32_t a = 0;
+			char ch;
+			while((ch = inp_()) != 0) {
+				if(ch >= '0' && ch <= '9') {
+					a <<= 4;
+					a += ch - '0';
+				} else if(ch >= 'A' && ch <= 'F') {
+					a <<= 4;
+					a += ch - 'A' + 10;
+				} else if(ch >= 'a' && ch <= 'f') {
+					a <<= 4;
+					a += ch - 'a' + 10;
+				} else {
+					break;
+				}
+			}
+			return a;
+		}
+
+
+		template<typename T>
+		T real_() {
+			uint32_t a = 0;
+			uint32_t b = 0;
+			uint32_t c = 1;
+			char ch;
+			bool p = false;
+			while((ch = inp_()) != 0) {
+				if(ch >= '0' && ch <= '9') {
+					a *= 10;
+					a += ch - '0';
+					c *= 10;
+				} else if(ch == '.') {
+					b = a;
+					a = 0;
+					c = 1;
+					p = true;
+				} else {
+					break;
+				}
+			}
+			if(p) {
+				return static_cast<T>(b) + static_cast<T>(a) / static_cast<T>(c);
+			} else {
+				return static_cast<T>(a); 
+			}
+		}
+
+
+		void next_()
+		{
+			enum class fmm : uint8_t {
+				none,
+				type,
+			};
+			fmm cm = fmm::none;
+
+			char ch;
+			while((ch = *form_++) != 0) {
+				switch(cm) {
+				case fmm::none:
+					if(ch == '[') {
+						auto a = inp_();
+						const char* p = form_;
+						bool ok = false;
+						while((ch = *p++) != 0 && ch != ']') {
+							if(ch == a) ok = true;
+						}
+						form_ = p;
+						if(!ok) {
+							error_ = error::cha_sets;
+							return;
+						}
+					} else if(ch == '%' && *form_ != '%') {
+						cm = fmm::type;
+					} else if(ch != inp_()) {
+						error_ = error::partition;
+						return;
+					}
+					break;
+
+				case fmm::type:
+					switch(ch) {
+					case 'b':
+						mode_ = mode::BIN;
+						break;
+					case 'o':
+						mode_ = mode::OCT;
+						break;
+					case 'd':
+						mode_ = mode::DEC;
+						break;
+					case 'x':
+						mode_ = mode::HEX;
+						break;
+					case 'f':
+						mode_ = mode::REAL;
+						break;
+					case 'c':
+						mode_ = mode::CHA;
+						break;
+					default:
+						error_ = error::input_type;
+						break;
+					}
+					return;
+				}
+			}
+			if(ch == 0 && inp_() == 0) ;
+			else {
+				error_ = error::terminate;
+			}
+		}
+
+
+		bool neg_() {
+			bool neg = false;
+			auto s = inp_();
+			if(s == '-') { neg = true; }
+			else if(s == '+') { neg = false; }
+			else inp_.unget();
+			return neg;
+		}
+
+
+		int32_t nb_int_(bool sign = true)
+		{
+			auto neg = neg_();
+
+			uint32_t v = 0;
+			switch(mode_) {
+			case mode::BIN:
+				v = bin_();
+				break;
+			case mode::OCT:
+				v = oct_();
+				break;
+			case mode::DEC:
+				v = dec_();
+				break;
+			case mode::HEX:
+				v = hex_();
+				break;
+			default:
+				error_ = error::not_integer;
+				break;
+			}
+			if(error_ == error::none) {
+				inp_.unget();
+				next_();
+				if(error_ == error::none) ++num_;
+			}
+			if(sign && neg) return -static_cast<int32_t>(v);
+			else return static_cast<int32_t>(v);
+		}
+
+
+		template <typename T>
+		T nb_real_()
+		{
+			bool neg = neg_();
+
+			T v = 0.0f;
+			switch(mode_) {
+			case mode::REAL:
+				v = real_<T>();
+				break;
+			default:
+				error_ = error::not_float;
+				break;
+			}
+			if(error_ == error::none) {
+				inp_.unget();
+				next_();
+				if(error_ == error::none) ++num_;
+			}
+			if(neg) return -v;
+			else return v;			
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+			@param[in]	form	フォーマット式
+			@param[in]	inp		変換文字列（nullptrの場合、sci_getch で取得）
+		*/
+		//-----------------------------------------------------------------//
+		basic_input(const char* form, const char* inp = nullptr) noexcept : form_(form), inp_(inp),
+			mode_(mode::NONE), error_(error::none), num_(0)
+		{
+			next_();
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  エラー種別を返す
+			@return エラー
+		*/
+		//-----------------------------------------------------------------//
+		error get_error() const noexcept { return error_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  変換ステータスを返す
+			@return 変換が全て正常なら「true」
+		*/
+		//-----------------------------------------------------------------//
+		bool status() const noexcept { return error_ == error::none; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  正常変換数を取得
+			@return 正常変換数
+		*/
+		//-----------------------------------------------------------------//
+		int num() const noexcept { return num_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  テンプレート・オペレーター「%」
+			@param[in]	val	整数型
+			@return	自分の参照
+		*/
+		//-----------------------------------------------------------------//
+		template <typename T>
+		basic_input& operator % (T& val) noexcept
+		{
+			if(error_ != error::none) return *this;
+
+			if(std::is_floating_point<T>::value) {
+				val = nb_real_<T>();
+			} else {
+				if(mode_ == mode::CHA) {
+					val = inp_();
+					next_();
+				} else {
+					val = nb_int_(std::is_signed<T>::value);
+				}
+			}
+			return *this;
+		}
+	};
+
+	typedef basic_input<def_chainp> input;
+}
