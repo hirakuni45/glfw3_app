1e83512c4d94479dac9037dd0900198e920cccaf
hira <hira@rvf-rc45.net>
2017-02-20 17:53:33 +0900

    update into header

--- glfw3_app/common/widgets/widget_terminal.hpp
+++ glfw3_app/common/widgets/widget_terminal.hpp
@@ -1,12 +1,14 @@
 #pragma once
 //=====================================================================//
 /*!	@file
-	@brief	GUI Widget ターミナル（ヘッダー）
+	@brief	GUI Widget ターミナル @n
+			Copyright 2017 Kunihito Hiramatsu
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
 #include "widgets/widget_director.hpp"
 #include "utils/terminal.hpp"
+#include "core/glcore.hpp"
 
 namespace gui {
 
@@ -156,7 +158,29 @@ namespace gui {
 			@brief	初期化
 		*/
 		//-----------------------------------------------------------------//
-		void initialize() override;
+		void initialize() override {
+			at_param().state_.set(widget::state::POSITION_LOCK);
+			at_param().state_.set(widget::state::SIZE_LOCK);
+			at_param().state_.set(widget::state::RESIZE_H_ENABLE, false);
+			at_param().state_.set(widget::state::RESIZE_V_ENABLE, false);
+			at_param().state_.set(widget::state::SERVICE);
+			at_param().state_.set(widget::state::MOVE_ROOT, false);
+			at_param().state_.set(widget::state::RESIZE_ROOT);
+			at_param().state_.set(widget::state::CLIP_PARENTS);
+			at_param().state_.set(widget::state::AREA_ROOT);
+
+			using namespace gl;
+			core& core = core::get_instance();
+			fonts& fonts = core.at_fonts();
+
+			fonts.push_font_face();
+			fonts.set_font_type(param_.font_);
+			fonts.set_font_size(param_.font_height_);
+			fonts.enable_proportional(false);
+			fonts.set_spaceing(0);
+			param_.font_width_ = fonts.get_width(' ');
+			fonts.pop_font_face();
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -164,7 +188,44 @@ namespace gui {
 			@brief	アップデート
 		*/
 		//-----------------------------------------------------------------//
-		void update() override;
+		void update() override {
+			if(wd_.get_focus_widget() == this || wd_.get_focus_widget() == wd_.root_widget(this)) {
+				focus_ = true;
+			} else {
+				focus_ = false;
+			}
+
+			if(get_param().parents_ && get_state(widget::state::AREA_ROOT)) {
+				if(get_param().parents_->type() == get_type_id<widget_frame>()) {
+					// 親になってるフレームを取得
+					widget_frame* w = static_cast<widget_frame*>(at_param().parents_);
+					if(w) {
+						bool resize = false;
+						vtx::irect sr = w->get_draw_area();
+						if(param_.auto_fit_) {
+							vtx::ipos ss(sr.size.x / param_.font_width_,
+										 sr.size.y / param_.height_);
+							ss.x *= param_.font_width_;
+//							if(ss.x < w->get_param().resize_min_.x) {
+//								ss.x = w->get_param().resize_min_.x / param_.font_width_;
+//								ss.x *= param_.font_width_;
+//							}
+							ss.y *= param_.height_;
+//							if(ss.y < w->get_param().resize_min_.y) {
+//								ss.y = w->get_param().resize_min_.y / param_.height_;
+//								ss.y *= param_.height_;
+//							}
+							w->set_draw_area(ss);
+							sr = w->get_draw_area();
+						}
+						if(sr.size != get_rect().size) resize = true;
+						at_rect() = sr;
+						if(resize) {
+						}
+					}
+				}
+			}
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -172,7 +233,24 @@ namespace gui {
 			@brief	サービス
 		*/
 		//-----------------------------------------------------------------//
-		void service() override;
+		void service() override {
+			if(focus_) {
+				if(param_.echo_) {
+					auto s = wd_.at_keyboard().input();
+					terminal_.output(s);
+					if(!s.empty()) {
+						if(param_.input_func_ != nullptr) {
+							for(auto ch : s) {
+								param_.input_func_(ch);
+							}
+						}
+						if(param_.enter_func_ != nullptr && terminal_.get_last_char() == 0x0D) {
+							param_.enter_func_(terminal_.get_last_text32());
+						}
+					}
+				}
+			}
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -180,7 +258,92 @@ namespace gui {
 			@brief	レンダリング
 		*/
 		//-----------------------------------------------------------------//
-		void render() override;
+		void render() override {
+			using namespace gl;
+			core& core = core::get_instance();
+			const vtx::spos& vsz = core.get_size();
+			const vtx::spos& siz = core.get_rect().size;
+			gl::fonts& fonts = core.at_fonts();
+
+			const widget::param& wp = get_param();
+
+			if(wp.clip_.size.x > 0 && wp.clip_.size.y > 0) {
+
+				glPushMatrix();
+
+				vtx::irect rect;
+				if(wp.state_[widget::state::CLIP_PARENTS]) {
+					rect.org  = wp.rpos_;
+					rect.size = wp.rect_.size;
+				} else {
+					rect.org.set(0);
+					rect.size = wp.rect_.size;
+				}
+
+				fonts.push_font_face();
+				fonts.set_font_type(param_.font_);
+				fonts.set_font_size(param_.font_height_);
+
+				vtx::irect clip_ = wp.clip_;
+
+				int sx = vsz.x / siz.x;
+				int sy = vsz.y / siz.y;
+				glViewport(clip_.org.x * sx, vsz.y - clip_.org.y * sy - clip_.size.y * sy,
+					clip_.size.x * sx, clip_.size.y * sy);
+				fonts.setup_matrix(clip_.size.x, clip_.size.y);
+
+				fonts.enable_center(false);
+				fonts.enable_proportional(false);
+
+				const img::rgbaf& cf = wd_.get_color();
+				vtx::ipos limit(clip_.size.x / param_.font_width_, clip_.size.y / param_.height_);
+				vtx::ipos chs(rect.org);
+				auto ln = terminal_.get_line_num();
+				vtx::ipos ofs(0);
+				if(ln > limit.y) ofs.y = ln - limit.y;
+				vtx::ipos pos;
+				for(pos.y = 0; pos.y < limit.y; ++pos.y) {
+					for(pos.x = 0; pos.x < limit.x; ++pos.x) {
+						const auto& t = terminal_.get_char(pos + ofs);
+						img::rgba8 fc = t.fc_;
+						fc *= cf.r;
+						fc.alpha_scale(cf.a);
+						fonts.set_fore_color(fc);
+						img::rgba8 bc = t.bc_;
+						bc *= cf.r;
+						bc.alpha_scale(cf.a);
+						fonts.set_back_color(bc);
+						if(focus_ && (pos + ofs) == terminal_.get_cursor()) {
+							if((interval_ % 40) < 20) {
+								fonts.swap_color();
+							}
+						}
+						auto cha = t.cha_;
+						if(cha < 0x20) cha = 0x3F;  // 制御コードは DEL-char として扱う
+						if(cha > 0x7f) {
+							fonts.pop_font_face();
+						}
+						int fw = fonts.get_width(cha);
+						vtx::irect br(chs, vtx::ipos(fw, param_.height_));
+						fonts.draw_back(br);
+						chs.x += fonts.draw(chs, cha);
+						fonts.swap_color(false);
+						if(cha > 0x7f) {
+							fonts.push_font_face();
+							fonts.set_font_type(param_.font_);
+						}
+					}
+					chs.y += param_.height_;
+					chs.x = rect.org.x;
+				}
+				++interval_;
+
+				fonts.restore_matrix();
+				fonts.pop_font_face();
+				glPopMatrix();
+				glViewport(0, 0, vsz.x, vsz.y);
+			}
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -190,7 +353,9 @@ namespace gui {
 			@return エラーが無い場合「true」
 		*/
 		//-----------------------------------------------------------------//
-		bool save(sys::preference& pre) override;
+		bool save(sys::preference& pre) override {
+			return false;
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -200,6 +365,8 @@ namespace gui {
 			@return エラーが無い場合「true」
 		*/
 		//-----------------------------------------------------------------//
-		bool load(const sys::preference& pre) override;
+		bool load(const sys::preference& pre) override {
+			return false;
+		}
 	};
 }
