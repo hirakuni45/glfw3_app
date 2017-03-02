3f7466059de49d18f21c4064d5c95cade2df7de1
hirakuni45 <hira@rvf-rc45.net>
2017-03-02 16:50:23 +0900

    into header

--- glfw3_app/piano_sim/piano_sim.hpp
+++ glfw3_app/piano_sim/piano_sim.hpp
@@ -1,6 +1,7 @@
 //=====================================================================//
 /*! @file
-	@brief  Piano SIM クラス
+	@brief  Piano SIM クラス @n
+			Copyright 2017 Kunihito Hiramatsu
 	@author 平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
@@ -46,7 +47,42 @@ namespace app {
 			@brief  初期化
 		*/
 		//-----------------------------------------------------------------//
-		void initialize();
+		void initialize()
+		{
+//			gl::core& core = gl::core::get_instance();
+
+			using namespace gui;
+			widget_director& wd = director_.at().widget_director_;
+
+
+			{ // ボタン
+				widget::param wp(vtx::irect(30, 30, 100, 40));
+				widget_button::param wp_("Ring");
+				test_ring_ = wd.add_widget<widget_button>(wp, wp_);
+			}
+
+
+			{	// ターミナル
+				{
+					widget::param wp(vtx::irect(10, 10, 200, 200));
+					widget_frame::param wp_;
+					wp_.plate_param_.set_caption(30);
+					terminal_frame_ = wd.add_widget<widget_frame>(wp, wp_);
+				}
+				{
+					widget::param wp(vtx::irect(0), terminal_frame_);
+					widget_terminal::param wp_;
+					terminal_core_ = wd.add_widget<widget_terminal>(wp, wp_);
+				}
+			}
+
+
+			// プリファレンスの取得
+			sys::preference& pre = director_.at().preference_;
+			if(terminal_frame_) {
+				terminal_frame_->load(pre);
+			}
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -54,7 +90,15 @@ namespace app {
 			@brief  アップデート
 		*/
 		//-----------------------------------------------------------------//
-		void update();
+		void update()
+		{
+			gui::widget_director& wd = director_.at().widget_director_;
+
+//			auto& sound = director_.at().sound_;
+//			audio_io::wave_handle request_pw(int slot, audio_format aform, const wave_gen_func& func);
+
+			wd.update();
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -62,7 +106,11 @@ namespace app {
 			@brief  レンダリング
 		*/
 		//-----------------------------------------------------------------//
-		void render();
+		void render()
+		{
+			director_.at().widget_director_.service();
+			director_.at().widget_director_.render();
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -70,8 +118,14 @@ namespace app {
 			@brief  廃棄
 		*/
 		//-----------------------------------------------------------------//
-		void destroy();
+		void destroy()
+		{
+			sys::preference& pre = director_.at().preference_;
 
+			if(terminal_frame_) {
+				terminal_frame_->save(pre);
+			}
+		}
 	};
 
 }
