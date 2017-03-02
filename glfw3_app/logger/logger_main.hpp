4836a771d8c5d5c64cd62358b57a31b9bd6952d7
hirakuni45 <hira@rvf-rc45.net>
2017-03-02 18:11:25 +0900

    into header

--- glfw3_app/logger/logger_main.hpp
+++ glfw3_app/logger/logger_main.hpp
@@ -1,6 +1,7 @@
 //=====================================================================//
 /*! @file
-	@brief  logger main クラス
+	@brief  logger main クラス @n
+			Copyright 2017 Kunihito Hiramatsu
 	@author 平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
@@ -45,7 +46,34 @@ namespace app {
 			@brief  初期化
 		*/
 		//-----------------------------------------------------------------//
-		void initialize();
+		void initialize()
+		{
+			gl::core& core = gl::core::get_instance();
+
+			using namespace gui;
+			widget_director& wd = director_.at().widget_director_;
+
+			{	// ターミナルのテスト
+				{
+					widget::param wp(vtx::irect(10, 10, 200, 200));
+					widget_frame::param wp_;
+					wp_.plate_param_.set_caption(30);
+					terminal_frame_ = wd.add_widget<widget_frame>(wp, wp_);
+				}
+				{
+					widget::param wp(vtx::irect(0), terminal_frame_);
+					widget_terminal::param wp_;
+					terminal_core_ = wd.add_widget<widget_terminal>(wp, wp_);
+				}
+			}
+
+
+			// プリファレンスの取得
+			sys::preference& pre = director_.at().preference_;
+			if(terminal_frame_) {
+				terminal_frame_->load(pre);
+			}
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -53,7 +81,12 @@ namespace app {
 			@brief  アップデート
 		*/
 		//-----------------------------------------------------------------//
-		void update();
+		void update()
+		{
+			gui::widget_director& wd = director_.at().widget_director_;
+
+			wd.update();
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -61,7 +94,11 @@ namespace app {
 			@brief  レンダリング
 		*/
 		//-----------------------------------------------------------------//
-		void render();
+		void render()
+		{
+			director_.at().widget_director_.service();
+			director_.at().widget_director_.render();
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -69,8 +106,13 @@ namespace app {
 			@brief  廃棄
 		*/
 		//-----------------------------------------------------------------//
-		void destroy();
+		void destroy()
+		{
+			sys::preference& pre = director_.at().preference_;
 
+			if(terminal_frame_) {
+				terminal_frame_->save(pre);
+			}
+		}
 	};
-
 }
