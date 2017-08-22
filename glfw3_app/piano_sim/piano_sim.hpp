bb35db73db9ce69316d9c094e842a09279e2e527
hira <hira@rvf-rc45.net>
2017-08-22 21:18:24 +0900

    update piano keys

--- glfw3_app/piano_sim/piano_sim.hpp
+++ glfw3_app/piano_sim/piano_sim.hpp
@@ -12,9 +12,12 @@
 #include "utils/director.hpp"
 #include "widgets/widget.hpp"
 #include "widgets/widget_button.hpp"
+#include "widgets/widget_slider.hpp"
 #include "widgets/widget_frame.hpp"
 #include "widgets/widget_terminal.hpp"
 #include "piano.hpp"
+
+#include "utils/format.hpp"
 
 namespace app {
 
@@ -26,9 +29,12 @@ namespace app {
 
 		gui::widget_button*		test_ring_;
 
+		gui::widget_button*		piano_keys_[48];
+
+		gui::widget_slider*		overtone_[8];
+
 		gui::widget_frame*		terminal_frame_;
 		gui::widget_terminal*	terminal_core_;
-
 
 
 		class piano_t {
@@ -43,18 +49,34 @@ namespace app {
 
 			uint32_t	reb_smp_;
 
+			void reberb_(float delay, float gain)
+			{
+//				uint32_t d = delay * static_cast<float>(sample_rate);
+				uint32_t d = 600;
+				if(reb_.size() < d) {
+					return;
+				}
+
+				int32_t g = gain * 8192;
+				for(int i = 0; i < wav_.size(); ++i) {
+					int idx = i - d;
+					int16_t w;
+					if(idx < 0) {
+						w =reb_[reb_.size() + idx];
+					} else {
+						w = wav_[idx];
+					}
+					int32_t tmp = wav_[i];
+					tmp += (static_cast<int32_t>(w) * g) / 8192;
+					tmp >>= 1;
+					wav_[i] = tmp;
+				}
+			}
+
 		public:
 			piano_t(const float* abc) : abc_(abc), frq_(0.0f), angle_{ 0.0f },
 				reb_(), wav_(),
 				reb_smp_(0) { }
-
-
-			void set_reverb(float rt)
-			{
-				reb_smp_ = static_cast<float>(sample_rate) * rt;
-				reb_.resize((sample_rate / 60) + reb_smp_);
-			}
-
 
 			void reset()
 			{
@@ -77,6 +99,7 @@ namespace app {
 					1.0f, 0.72f, 0.55f, 0.49f, 0.73f, 0.52f, 0.2f, 0.15f 
 				};
 
+				reb_ = wav_;
 				wav_.resize(len);
 				for(int i = 0; i < len; ++i) {
 					float l = 0.0f;
@@ -88,6 +111,9 @@ namespace app {
 					l *= gain;
 					wav_[i] = static_cast<int16_t>(l * 10000.0f);
 				}
+
+				// リバーブ処理				
+				reberb_(50e-3, 0.6f);
 			}
 
 			const al::sound::waves16& get_wav() const { return wav_; }
@@ -125,6 +151,89 @@ namespace app {
 			key_[12] = dev.get_level(gl::device::key::COMMA);
 		}
 
+		void init_piano_key_(int x, int o)
+		{
+			gl::core& core = gl::core::get_instance();
+
+			using namespace gui;
+			widget_director& wd = director_.at().widget_director_;
+
+			const std::string& curp = core.get_current_path();
+			vtx::irect r(x, 400, 0, 0);
+			widget_button* wb;
+
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_c.png", r);  // C
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[0 + o] = wb;
+
+			r.org.x = x + 24;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_d.png", r);  // D
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[2 + o] = wb;
+
+			r.org.x = x + 14;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_cs.png", r); // C#
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[1 + o] = wb;
+
+			r.org.x = x + 24 + 24 + 1;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_e.png", r);  // E
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[4 + o] = wb;
+
+			r.org.x = x + 43;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_cs.png", r);  // D#
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[3 + o] = wb;
+
+			r.org.x = x + 48 + 24 + 1;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_f.png", r);  // F
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[5 + o] = wb;
+
+			r.org.x = x + 48 + 24 + 1 + 24 + 1;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_g.png", r);  // G
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[7 + o] = wb;
+
+			r.org.x = x + 85;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_cs.png", r);  // F#
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[6 + o] = wb;
+
+			r.org.x = x + 122;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_a.png", r);  // A
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[9 + o] = wb;
+
+			r.org.x = x + 114;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_cs.png", r);  // G#
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[8 + o] = wb;
+
+			r.org.x = x + 146;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_a.png", r);  // B
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[11 + o] = wb;
+
+			r.org.x = x + 140;
+			wb = gui::create_image<widget_button>(wd, curp + "/res/piano_key_cs.png", r);  // A#
+			wb->at_param().action_.set(widget::action::SELECT_SCALE, false);
+			wb->at_param().action_.set(widget::action::SELECT_HIGHLIGHT);
+			piano_keys_[10 + o] = wb;
+		}
+
 	public:
 		//-----------------------------------------------------------------//
 		/*!
@@ -132,6 +241,8 @@ namespace app {
 		*/
 		//-----------------------------------------------------------------//
 		piano_sim(utils::director<core>& d) : director_(d),
+			test_ring_(nullptr), piano_keys_{ nullptr },
+			overtone_{ nullptr },
 			terminal_frame_(nullptr), terminal_core_(nullptr),
 			abc_{ 0.0f }, gain_(0.0f),
 			piano_(abc_), env_(0.0f),
@@ -159,10 +270,43 @@ namespace app {
 			widget_director& wd = director_.at().widget_director_;
 
 
-			{ // ボタン
+			{  // ボタン
 				widget::param wp(vtx::irect(30, 30, 100, 40));
 				widget_button::param wp_("Ring");
 				test_ring_ = wd.add_widget<widget_button>(wp, wp_);
+			}
+
+			{  // 倍音スライダー
+				for(int i = 0; i < 8; ++i) {
+					widget::param wp(vtx::irect(30, 100 + 30 * i, 180, 20));
+					widget_slider::param wp_;
+//					wp_.select_func_ = [this] (float lvl) {
+//						if(progress_ != nullptr) {
+//							progress_->set_ratio(lvl);
+//						}
+//					};
+					overtone_[i] = wd.add_widget<widget_slider>(wp, wp_);
+				}
+			}
+
+
+			{ // 鍵盤
+				init_piano_key_(10+170*0, 12 * 0);
+				init_piano_key_(10+170*1, 12 * 1);
+				init_piano_key_(10+170*2, 12 * 2);
+				init_piano_key_(10+170*3, 12 * 3);
+#if 0
+				file_btn_->at_local_param().select_func_ = [this](int id) {
+					bool f = filer_->get_state(gui::widget::state::ENABLE);
+					filer_->enable(!f);
+					if(!f) {
+						auto& sound = director_.at().sound_;
+						filer_->focus_file(sound.get_file_stream());
+						files_step_ = 0;
+						files_.clear();
+					}
+				};
+#endif
 			}
 
 
@@ -208,6 +352,13 @@ namespace app {
 
 			keys_();
 
+			for(int i = 0; i < 48; ++i) {
+				if(piano_keys_[i]->get_select_in()) {
+					utils::format("%d\n") % i;
+				}
+			}
+
+
 			al::sound& sound = director_.at().sound_;
 
 			// Envelope
@@ -216,7 +367,6 @@ namespace app {
 					gain_ = 1.0f;
 					env_ = 0.965f;
 					piano_.reset();
-					
 				}
 				if(test_ring_->get_select_out()) {
 					++idx_;
@@ -242,7 +392,6 @@ namespace app {
 
 			float vol = 0.75f;
 			sound.set_gain_stream(vol);
-
 
 			wd.update();
 		}
