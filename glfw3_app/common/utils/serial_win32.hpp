b48e1c1a257741efe32c10021e1d8a6c94f0c06f
hira <hira@rvf-rc45.net>
2017-12-28 07:27:05 +0900

    update

--- glfw3_app/common/utils/serial_win32.hpp
+++ glfw3_app/common/utils/serial_win32.hpp
@@ -2,8 +2,10 @@
 //=====================================================================//
 /*!	@file
 	@brief	シリアルＩ／Ｏ（WIN32）
-			Copyright 2017 Kunihito Hiramatsu
-	@author	平松邦仁 (hira@rvf-rc45.net)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/glfw_app/blob/master/LICENSE
 */
 //=====================================================================//
 
