f794567b632f3cb00dcd5259ae001a271885c537
hira <hira@rvf-rc45.net>
2018-02-05 07:29:55 +0900

    move: wave render, tools

---
+++ glfw3_app/ignitor/wave_cap.hpp
@@ -0,0 +1,206 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  波形キャプチャー・クラス
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "gl_fw/render_waves.hpp"
+
+namespace app {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  wave_cap クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class wave_cap {
+
+		utils::director<core>&	director_;
+
+		typedef view::render_waves<uint16_t, 4096, 4> WAVES;
+		WAVES					waves_;
+
+		gui::widget_frame*		tools_;
+
+		gui::widget_frame*		frame_;
+		gui::widget_view*		core_;
+
+#if 0
+				wp_.init_list_.push_back("1000 ms");
+				wp_.init_list_.push_back("500 ms");
+				wp_.init_list_.push_back("250 ms");
+				wp_.init_list_.push_back("100 ms");
+				wp_.init_list_.push_back("50 ms");
+				wp_.init_list_.push_back("10 ms");
+				wp_.init_list_.push_back("5 ms");
+				wp_.init_list_.push_back("1 ms");
+				wp_.init_list_.push_back("500 us");
+				wp_.init_list_.push_back("100 us");
+				wp_.init_list_.push_back("50 us");
+				wp_.init_list_.push_back("25 us");
+				wp_.init_list_.push_back("10 us");
+				wp_.init_list_.push_back("5 us");
+				wp_.init_list_.push_back("1 us");
+#endif
+		static constexpr double div_tbls_[] = {
+			1000e-3,
+			500e-3,
+			250e-3,
+			100e-3,
+			50e-3,
+			10e-3,
+			5e-3,
+			1e-3,
+			500e-6,
+			100e-6,
+			50e-6,
+			25e-6,
+			10e-6,
+			5e-6,
+			1e-6,
+		};
+
+		// 波形描画
+		void update_view_()
+		{
+		}
+
+
+		void render_view_(const vtx::irect& clip)
+		{
+//			gui::widget_director& wd = director_.at().widget_director_;
+
+			glDisable(GL_TEXTURE_2D);
+
+//			auto pos = div_->get_menu()->get_select_pos();
+			waves_.render(clip.size, 65536);
+
+			glEnable(GL_TEXTURE_2D);
+		}
+
+
+		void service_view_()
+		{
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+		*/
+		//-----------------------------------------------------------------//
+		wave_cap(utils::director<core>& d) : director_(d),
+			waves_(), tools_(nullptr), frame_(nullptr), core_(nullptr)
+		{ }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  初期化
+		*/
+		//-----------------------------------------------------------------//
+		void initialize()
+		{
+			using namespace gui;
+			widget_director& wd = director_.at().widget_director_;
+
+			{	// 波形ビュー
+				widget::param wp(vtx::irect(40, 150, 200, 200));
+				widget_frame::param wp_;
+				wp_.plate_param_.set_caption(12);
+				frame_ = wd.add_widget<widget_frame>(wp, wp_);
+			}
+			{
+				widget::param wp(vtx::irect(0), frame_);
+				widget_view::param wp_;
+				wp_.update_func_ = [=]() {
+					update_view_();
+				};
+				wp_.render_func_ = [=](const vtx::irect& clip) {
+					render_view_(clip);
+				};
+				wp_.service_func_ = [=]() {
+					service_view_();
+				};
+				core_ = wd.add_widget<widget_view>(wp, wp_);
+			}
+
+			sys::preference& pre = director_.at().preference_;
+			if(frame_ != nullptr) {
+				frame_->load(pre);
+			}
+			if(tools_ != nullptr) {
+				tools_->load(pre);
+			}
+
+			// テスト波形生成
+///			waves_.create_buffer(0.5, 10e-6);
+
+//			waves_.at_param(0).gain_ = 0.025f;
+//			waves_.at_param(1).gain_ = 0.025f;
+//			waves_.at_param(0).color_ = img::rgba8(255, 128, 255, 255);
+//			waves_.at_param(1).color_ = img::rgba8(128, 255, 255, 255);
+//			waves_.at_param(0).offset_ = 0;
+//			waves_.at_param(1).offset_ = 200;
+
+			waves_.at_info().time_org_ = 50;
+			waves_.at_info().time_len_ = 150;
+
+			waves_.at_info().volt_org_ = 80;
+			waves_.at_info().volt_len_ = 130;
+///			waves_.build_sin(10e3);
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  アップデート
+		*/
+		//-----------------------------------------------------------------//
+		void update()
+		{
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ロード
+		*/
+		//-----------------------------------------------------------------//
+		void load()
+		{
+			sys::preference& pre = director_.at().preference_;
+
+			if(frame_ != nullptr) {
+				frame_->load(pre);
+			}
+			if(tools_ != nullptr) {
+				tools_->load(pre);
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  セーブ
+		*/
+		//-----------------------------------------------------------------//
+		void save()
+		{
+			sys::preference& pre = director_.at().preference_;
+
+			if(frame_ != nullptr) {
+				frame_->save(pre);
+			}
+			if(tools_ != nullptr) {
+				tools_->save(pre);
+			}
+		}
+	};
+}
+
+
