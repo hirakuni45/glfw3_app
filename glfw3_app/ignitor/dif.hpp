2430ec3569b6edba5f2f9c6dc8d0f7a7b16f6fe4
hirakuni45 <hira@rvf-rc45.net>
2018-03-21 14:07:50 +0900

    update: dif manage

--- glfw3_app/ignitor/dif.hpp
+++ glfw3_app/ignitor/dif.hpp
@@ -37,13 +37,13 @@ namespace app {
 	{
 		utils::director<core>&	director_;
 
-	public:
-
-
-
-	private:
-
-
+		gui::widget_label*		tag1_;	///< 検査記号１
+		gui::widget_label*		val1_;	///< 値１
+		gui::widget_list*		calc_;	///< 計算モード
+		gui::widget_label*		tag2_;	///< 検査記号２
+		gui::widget_label*		val2_;	///< 値２
+		gui::widget_label*		ans_;	///< 答え
+		gui::widget_button*		exec_;	///< 計算
 
 	public:
 		//-----------------------------------------------------------------//
@@ -52,9 +52,10 @@ namespace app {
 		*/
 		//-----------------------------------------------------------------//
 		dif(utils::director<core>& d) :
-			director_(d)
+			director_(d),
+			tag1_(nullptr), val1_(nullptr), calc_(nullptr), tag2_(nullptr), val2_(nullptr),
+			ans_(nullptr), exec_(nullptr)
 		{ }
-
 
 
 		//-----------------------------------------------------------------//
@@ -70,6 +71,67 @@ namespace app {
 		{
 			using namespace gui;
 			widget_director& wd = director_.at().widget_director_;
+
+			{
+				widget::param wp(vtx::irect(15, ofsy, 120, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_text::param wp_("差分検査:");
+				wp_.text_param_.placement_ = vtx::placement(vtx::placement::holizontal::LEFT,
+											 vtx::placement::vertical::CENTER);
+				wd.add_widget<widget_text>(wp, wp_);
+			}
+
+			ofsx += 50;
+			{  // TAG1
+				widget::param wp(vtx::irect(ofsx, ofsy, 100, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_label::param wp_("", false);
+				tag1_ = wd.add_widget<widget_label>(wp, wp_);
+//				tag1_->at_local_param().select_func_ = [=](const std::string& str) {
+//				};
+			}
+			{  // VAL1
+				widget::param wp(vtx::irect(ofsx, ofsy + 50, 100, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_label::param wp_("");
+				val1_ = wd.add_widget<widget_label>(wp, wp_);
+			}
+			{
+				widget::param wp(vtx::irect(ofsx + 110, ofsy, 70, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_list::param wp_;
+				wp_.init_list_.push_back("－");
+				wp_.init_list_.push_back("＋");
+				calc_ = wd.add_widget<widget_list>(wp, wp_);
+			}
+			{  // TAG2
+				widget::param wp(vtx::irect(ofsx + 190, ofsy, 100, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_label::param wp_("", false);
+				tag2_ = wd.add_widget<widget_label>(wp, wp_);
+//				tag2_->at_local_param().select_func_ = [=](const std::string& str) {
+//				};
+			}
+			{  // VAL2
+				widget::param wp(vtx::irect(ofsx + 190, ofsy + 50, 100, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_label::param wp_("");
+				val2_ = wd.add_widget<widget_label>(wp, wp_);
+			}
+			{  // 答え
+				widget::param wp(vtx::irect(ofsx + 300, ofsy + 50, 100, 40), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_label::param wp_("");
+				ans_ = wd.add_widget<widget_label>(wp, wp_);
+			}
+			{
+				widget::param wp(vtx::irect(d_w - 60, ofsy + 50, 30, 30), root);
+				wp.pre_group_ = widget::PRE_GROUP::_1;
+				widget_button::param wp_(">");
+				exec_ = wd.add_widget<widget_button>(wp, wp_);
+				exec_->at_local_param().select_func_ = [=](int n) {
+				};
+			}
 		}
 
 
@@ -80,6 +142,26 @@ namespace app {
 		//-----------------------------------------------------------------//
 		void update()
 		{
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ヘルプ機能
+			@param[in]	chip	ヘルプ・チップ
+		*/
+		//-----------------------------------------------------------------//
+		bool help(gui::widget_chip* chip)
+		{
+			bool ret = true;
+			if(tag1_->get_focus()) {
+				tools::set_help(chip, tag1_, "検査記号１");
+			} else if(tag2_->get_focus()) {
+				tools::set_help(chip, tag2_, "検査記号２");
+			} else {
+				ret = false;
+			}
+			return ret;
 		}
 
 
