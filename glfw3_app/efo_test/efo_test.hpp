777d31f6c798855973d0011783550a08a6fba056
hira <hira@rvf-rc45.net>
2018-07-13 06:28:20 +0900

    update: autotest, wave position, wave gain

--- glfw3_app/efo_test/efo_test.hpp
+++ glfw3_app/efo_test/efo_test.hpp
@@ -27,6 +27,8 @@
 
 #include "utils/fixed_fifo.hpp"
 #include "utils/serial_win32.hpp"
+#include "utils/file_io.hpp"
+#include "utils/input.hpp"
 
 namespace app {
 
@@ -50,6 +52,11 @@ namespace app {
 		gui::widget_spinbox*	ch2_gain_;
 
 		gui::widget_button*		single_;
+		gui::widget_button*		autotest_;
+		gui::widget_label*		autotest_min_;
+		gui::widget_label*		autotest_max_;
+		gui::widget_label*		autotest_result_;
+
 		gui::widget_button*		capture_;
 		gui::widget_list*		slope_;
 		gui::widget_label*		level_;
@@ -71,6 +78,11 @@ namespace app {
 		SERIAL::name_list		serial_list_;
 		typedef utils::fixed_fifo<uint8_t, 8192> FIFO;
 		FIFO					fifo_;
+
+		bool					autotest_enable_;
+		uint32_t				autotest_count_;
+		utils::file_io			autotest_fp_;
+		uint32_t				autotest_fail_;
 
 		double get_div_rate_()
 		{
@@ -142,6 +154,16 @@ namespace app {
 		}
 
 
+		bool get_float_(const std::string& str, float& ans) {
+			float a;
+			auto f = (utils::input("%f", str.c_str()) % a).status();
+			if(f) {
+				ans = a;
+			}
+			return f;
+		}
+
+
 		void service_waves_()
 		{
 			uint8_t tmp[256];
@@ -165,9 +187,33 @@ namespace app {
 						float a = static_cast<int16_t>(w);
 						a /= 32767.0f;
 						a *= 20.48f;
-						auto s = (boost::format("Value (ch%d): %4.2f [V] (%d)\n")
-							% ch % a % static_cast<int16_t>(w)).str();
+						auto s = (boost::format("%d Value (ch%d): %4.2f [V] (%d)")
+							% autotest_count_ % static_cast<int>(ch)
+							% a % static_cast<int16_t>(w)).str();
+						s += '\n';
+						if(!autotest_fp_.is_open()) {
+							autotest_fp_.open("autotest.txt", "wb");
+						}
+						autotest_fp_.write(s);
 						terminal_core_->output(s);
+						float min = 0.0f;
+						float max = 0.0f;
+						if(get_float_(autotest_min_->get_text(), min)) {
+							if(get_float_(autotest_max_->get_text(), max)) {
+								auto d = max - min;
+								if(d > 0.0f) {
+									if(min <= a && a <= max) {
+									} else {
+										++autotest_fail_;
+									}
+								}
+							}
+						}
+						{
+							std::string s;
+							s = (boost::format("%d/%d") % autotest_fail_ % autotest_count_).str();
+							autotest_result_->set_text(s);
+						}
 						wave_task_ = wave_task::idle;
 					} else if(cmd == 0x02) { // MULTI data
 						auto len_h = fifo_.get();
@@ -222,6 +268,19 @@ namespace app {
 			}
 		}
 
+
+		void send_single_()
+		{
+			uint8_t tmp[6];
+			tmp[0] = 0x01;  // START
+			tmp[1] = 0x00;  // Length(H)
+			tmp[2] = 0x00;  // LENGTH(L)
+			tmp[3] = 0x00;  // Volt(H)
+			tmp[4] = 0x00;  // Volt(L)
+			tmp[5] = 0x01;  // Edge
+			serial_.write(tmp, sizeof(tmp));
+		}
+
 	public:
 		//-----------------------------------------------------------------//
 		/*!
@@ -236,13 +295,19 @@ namespace app {
 			ch1_pos_(nullptr), ch2_pos_(nullptr),
 			ch1_gain_(nullptr), ch2_gain_(nullptr),
 
-			single_(nullptr),
+			single_(nullptr), autotest_(nullptr), autotest_min_(nullptr), autotest_max_(nullptr),
+			autotest_result_(nullptr),
+
 			capture_(nullptr),
 			slope_(nullptr), level_(nullptr), trigger_(nullptr),
 			wave_(nullptr),
 			terminal_frame_(nullptr), terminal_core_(nullptr),
 			view_frame_(nullptr), view_core_(nullptr),
-			waves_(), serial_(), serial_list_(), fifo_(), wave_task_(wave_task::idle),
+			waves_(), serial_(), serial_list_(), fifo_(),
+
+			autotest_enable_(false), autotest_count_(0), autotest_fp_(), autotest_fail_(0),
+
+			wave_task_(wave_task::idle),
 			wave_ch_(0), wave_max_(0), wave_pos_(0), wave_ch0_{ 0 }, wave_ch1_{ 0 }
 		{ }
 
@@ -259,7 +324,7 @@ namespace app {
 			gl::core& core = gl::core::get_instance();
 
 			int menu_width  = 130;
-			int menu_height = 600;
+			int menu_height = 750;
 			{	// メニューパレット
 				widget::param wp(vtx::irect(10, 10, menu_width, menu_height));
 				widget_frame::param wp_;
@@ -364,19 +429,40 @@ namespace app {
 				widget_button::param wp_("single");
 				single_ = wd.add_widget<widget_button>(wp, wp_);
 				single_->at_local_param().select_func_ = [=](int id) {
-					uint8_t tmp[6];
-					tmp[0] = 0x01;  // START
-					tmp[1] = 0x00;  // Length(H)
-					tmp[2] = 0x00;  // LENGTH(L)
-					tmp[3] = 0x00;  // Volt(H)
-					tmp[4] = 0x00;  // Volt(L)
-					tmp[5] = 0x01;  // Edge
-					serial_.write(tmp, sizeof(tmp));
+					send_single_();
 				};
+			}
+			{ // オートテスト・ボタン
+				widget::param wp(vtx::irect(10, 20+40*9, menu_width - 20, 30), menu_);
+				widget_button::param wp_("autotest");
+				autotest_ = wd.add_widget<widget_button>(wp, wp_);
+				autotest_->at_local_param().select_func_ = [=](int id) {
+					bool f = autotest_enable_;
+					autotest_enable_ = !autotest_enable_;
+					if(!f && autotest_enable_) {
+						autotest_count_ = 0;
+						autotest_fail_ = 0;
+					} 
+				};
+			}
+			{  // オートテスト最小
+				widget::param wp(vtx::irect(10, 20+40*10, menu_width - 20, 30), menu_);
+				widget_label::param wp_("0", false);
+				autotest_min_ = wd.add_widget<widget_label>(wp, wp_);
+			}
+			{  // オートテスト最大
+				widget::param wp(vtx::irect(10, 20+40*11, menu_width - 20, 30), menu_);
+				widget_label::param wp_("0", false);
+				autotest_max_ = wd.add_widget<widget_label>(wp, wp_);
+			}
+			{  // オートテスト結果
+				widget::param wp(vtx::irect(10, 20+40*12, menu_width - 20, 30), menu_);
+				widget_label::param wp_("");
+				autotest_result_ = wd.add_widget<widget_label>(wp, wp_);
 			}
 
 			{  // キャプチャー・ボタン
-				widget::param wp(vtx::irect(10, 20+40*9, menu_width - 20, 30), menu_);
+				widget::param wp(vtx::irect(10, 20+40*14, menu_width - 20, 30), menu_);
 				widget_button::param wp_("capture");
 				capture_ = wd.add_widget<widget_button>(wp, wp_);
 				capture_->at_local_param().select_func_ = [=](int fd) {
@@ -387,7 +473,7 @@ namespace app {
 			}
 
 			{  // スロープ選択
-				widget::param wp(vtx::irect(10, 20+40*10, menu_width - 20, 30), menu_);
+				widget::param wp(vtx::irect(10, 20+40*15, menu_width - 20, 30), menu_);
 				widget_list::param wp_;
 				wp_.init_list_.push_back("pos");
 				wp_.init_list_.push_back("neg");
@@ -395,13 +481,13 @@ namespace app {
 			}
 
 			{  // トリガー・レベル
-				widget::param wp(vtx::irect(10, 20+40*11, menu_width - 20, 30), menu_);
+				widget::param wp(vtx::irect(10, 20+40*16, menu_width - 20, 30), menu_);
 				widget_label::param wp_("0");
 				level_ = wd.add_widget<widget_label>(wp, wp_);
 			}
 
 			{ // トリガー・ボタン
-				widget::param wp(vtx::irect(10, 20+40*12, menu_width - 20, 30), menu_);
+				widget::param wp(vtx::irect(10, 20+40*17, menu_width - 20, 30), menu_);
 				widget_button::param wp_("trigger");
 				capture_ = wd.add_widget<widget_button>(wp, wp_);
 				capture_->at_local_param().select_func_ = [=](int fd) {
@@ -483,6 +569,13 @@ namespace app {
 			if(slope_ != nullptr) slope_->load(pre); 
 			if(level_ != nullptr) level_->load(pre); 
 
+			if(ch1_pos_ != nullptr) ch1_pos_->load(pre);
+			if(ch2_pos_ != nullptr) ch2_pos_->load(pre);
+			if(ch1_gain_ != nullptr) ch1_gain_->load(pre);
+			if(ch2_gain_ != nullptr) ch2_gain_->load(pre);
+			if(autotest_min_ != nullptr) autotest_min_->load(pre);
+			if(autotest_max_ != nullptr) autotest_max_->load(pre);
+
 			// 波形バッファ生成
 			waves_.create_buffer();
 
@@ -531,6 +624,17 @@ namespace app {
 
 			service_waves_();
 
+			if(autotest_enable_) {
+				static uint32_t count = 0;
+				++count;
+				if(count >= 15) {
+					send_single_();
+					++autotest_count_;
+					count = 0;
+				}
+			}
+
+
 			wd.update();
 		}
 
@@ -554,7 +658,18 @@ namespace app {
 		//-----------------------------------------------------------------//
 		void destroy()
 		{
+			if(autotest_fp_.is_open()) {
+				autotest_fp_.close();
+			}
+
 			sys::preference& pre = director_.at().preference_;
+
+			if(ch1_pos_ != nullptr) ch1_pos_->save(pre);
+			if(ch2_pos_ != nullptr) ch2_pos_->save(pre);
+			if(ch1_gain_ != nullptr) ch1_gain_->save(pre);
+			if(ch2_gain_ != nullptr) ch2_gain_->save(pre);
+			if(autotest_min_ != nullptr) autotest_min_->save(pre);
+			if(autotest_max_ != nullptr) autotest_max_->save(pre);
 
 			if(slope_ != nullptr) slope_->save(pre); 
 			if(level_ != nullptr) level_->save(pre); 
