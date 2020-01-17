cd2951b6a3e3b009698d8071e1568d128450b93c
hirakuni45 <hira@rvf-rc45.net>
2020-01-18 03:49:18 +0900

    Update: Change JoystickAxis to JoystickHat

--- glfw3_app/common/core/device.cpp
+++ glfw3_app/common/core/device.cpp
@@ -23,19 +23,32 @@ namespace gl {
 	void device::service(const bits_t& bits, const locator& poss)
 	{
 		bits_t b = bits;
-		int joy = 0;
+		int joy = GLFW_JOYSTICK_1;  // first JOY-STICK
 		if(glfwJoystickPresent(joy) == GL_TRUE) {
 			int count;
-			const float* axes = glfwGetJoystickAxes(joy, &count);
-			for(int i = 0; i < count; ++i) {
-				if(i == 1) {
-					if(axes[i] > 0.5f) b.set(key::GAME_UP);
-					else if(axes[i] < -0.5f) b.set(key::GAME_DOWN);
-				} else if(i == 0) {
-					if(axes[i] > 0.5f) b.set(key::GAME_RIGHT);
-					else if(axes[i] < -0.5f) b.set(key::GAME_LEFT);
-				}
+			const unsigned char* hats = glfwGetJoystickHats(joy, &count);
+			if((hats[0] & GLFW_HAT_UP) != 0) {
+				b.set(key::GAME_UP);
 			}
+			if((hats[0] & GLFW_HAT_DOWN) != 0) {
+				b.set(key::GAME_DOWN);
+			}
+			if((hats[0] & GLFW_HAT_LEFT) != 0) {
+				b.set(key::GAME_LEFT);
+			}
+			if((hats[0] & GLFW_HAT_RIGHT) != 0) {
+				b.set(key::GAME_RIGHT);
+			}
+//			const float* axes = glfwGetJoystickAxes(joy, &count);
+//			for(int i = 0; i < count; ++i) {
+//				if(i == 1) {
+//					if(axes[i] > 0.5f) b.set(key::GAME_UP);
+//					else if(axes[i] < -0.5f) b.set(key::GAME_DOWN);
+//				} else if(i == 0) {
+//					if(axes[i] > 0.5f) b.set(key::GAME_RIGHT);
+//					else if(axes[i] < -0.5f) b.set(key::GAME_LEFT);
+//				}
+//			}
 			const unsigned char* bl = glfwGetJoystickButtons(joy, &count);
 			if(count > 16) count = 16;
 			for(int i = 0; i < count; ++i) {
