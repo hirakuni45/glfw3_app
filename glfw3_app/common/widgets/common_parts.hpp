10216b96d66cb36fdf07fc9e63307a9983f26b4e
hirakuni45 <hira@rvf-rc45.net>
2023-01-28 20:14:37 +0900

    Update: move to header

--- glfw3_app/common/widgets/common_parts.hpp
+++ glfw3_app/common/widgets/common_parts.hpp
@@ -3,16 +3,20 @@
 /*!	@file
 	@brief	GUI widget 共通パーツ（ヘッダー）
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2023 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/glfw_app/blob/master/LICENSE
 */
 //=====================================================================//
-#include "gl_fw/glmobj.hpp"
-#include "widgets/widget.hpp"
 #include <boost/unordered_map.hpp>
+#include "gl_fw/glmobj.hpp"
+#include "img_io/paint.hpp"
+#include "widgets/widget.hpp"
 
 namespace gui {
+
+	void create_round_frame(img::paint& pa, const widget::plate_param& pp,
+		const widget::color_param& cp, const vtx::ipos& size);
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
@@ -41,16 +45,6 @@ namespace gui {
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief	共有ハンドルを追加
-			@param[in]	k	キー
-			@return	描画ハンドル
-		*/
-		//-----------------------------------------------------------------//
-		gl::mobj::handle add(const share_t& k);
-
-
-		//-----------------------------------------------------------------//
-		/*!
 			@brief	共有ハンドルを取得
 			@param[in]	k	キー
 			@return	共有ハンドル
@@ -62,6 +56,34 @@ namespace gui {
 			if(cit != share_map_.end()) {
 				h = cit->second;
 			}
+			return h;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	共有ハンドルを追加
+			@param[in]	k	キー
+			@return	描画ハンドル
+		*/
+		//-----------------------------------------------------------------//
+		gl::mobj::handle add(const share_t& k)
+		{
+			gl::mobj::handle h = get(k);
+			if(h == 0) {
+				img::paint pa;
+				create_round_frame(pa, k.plate_param_, k.color_param_, k.size_);
+				if(k.plate_param_.resizeble_) {
+// std::cout << "grid: " << static_cast<int>(k.plate_param_.grid_.x) << ", " << static_cast<int>(k.plate_param_.grid_.y) << std::endl;
+// std::cout << "size: " << static_cast<int>(pa.get_size().x) << ", " << static_cast<int>(pa.get_size().y) << std::endl;
+					h = mobj_.install(&pa, k.size_, k.plate_param_.grid_);
+				} else {
+					h = mobj_.install(&pa);
+				}
+				std::pair<share_t, handle> t(k, h);
+				share_map_.insert(t);
+			}
+// std::cout << static_cast<int>(h) << std::endl;
 			return h;
 		}
 	};
